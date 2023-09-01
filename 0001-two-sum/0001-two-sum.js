5d1271ab198e8f7979df29dce484e10f475dec66
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-01 15:41:36 +0900

    Time: 58 ms (80.75%), Space: 42.6 MB (52.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-    for (var j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    const map = new Map();
+    for(let i=0; i<nums.length; i++){
+        const complement = target - nums[i];
+        if(map.has(complement))
+            return [map.get(complement), i];
+        map.set(nums[i], i);
     }
-  }
-
-  return []; // no answer
+    return [];
 };
