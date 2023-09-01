23350181e3a848822ae6c95cc47d184db72a7f41
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-02 00:07:17 +0900

    Time: 82 ms (87.36%), Space: 46.9 MB (88.54%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -10,21 +10,12 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-  const node = new ListNode();
-  let tmpNode = node;
-  let carry = 0;
-  while (l1 || l2 || carry) {
-    tmpNode.next = new ListNode();
-    tmpNode = tmpNode.next;
-    const left = l1 ? l1.val : 0;
-    const right = l2 ? l2.val : 0;
-    let sum = left + right + carry;
-    const value = sum < 10 ? sum : sum % 10;
-    carry = sum < 10 ? 0 : 1;
-    tmpNode.val = value;
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
-  }
-  return node.next;
+var addTwoNumbers = function(l1, l2) {
+    const iter = (n1, n2, rest = 0) => {
+        if (!n1 && !n2 && !rest) return null;
+        const newVal = (n1?.val || 0) + (n2?.val || 0) + rest;
+        const nextNode = iter(n1?.next, n2?.next, Math.floor(newVal / 10));
+        return new ListNode(newVal % 10, nextNode);
+    }
+    return iter(l1, l2);
 };
