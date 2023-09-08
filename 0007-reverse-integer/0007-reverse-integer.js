3d2b71b4549790330805a839753e91d6842f635b
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-08 13:46:36 +0900

    Time: 57 ms (83.59%), Space: 43.5 MB (82.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var reverse = function(x) {
-    if(x===0)return 0;
-    if(x>0){
-        let result = parseInt(x.toString().split('').reverse().join(''));
-        return result < Math.pow(2,31)-1?result:0;
-    }else{
-        let result = x.toString().split('');
-        result.push('-');
-        result.shift();
-        result = result.reverse()
-        result = parseInt(result.join(''));
-        return result > Math.pow(-2,31)?result:0;
-    }
-};
+  let rev = 0;
+  const sign = x < 0 ? -1 : 1;
+  x = Math.abs(x);
+  while (x !== 0) {
+    const digit = x % 10;
+    rev = rev * 10 + digit;
+    x = Math.floor(x / 10);
+  }
+  const result = sign * rev;
+  if (result > 2 ** 31 - 1 || result < -(2 ** 31)) return 0;
+  return result;
+}
