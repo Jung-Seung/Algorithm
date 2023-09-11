4c78b37f9c1108884b40caf819c5f655db666eb7
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-11 15:44:00 +0900

    Time: 143 ms (63%), Space: 49.9 MB (96.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,7 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    var s = x.toString();
-    var t = s.split("").reverse().join("");
-    return s === t;
+    if (x < 0 || (x !== 0 && x % 10 === 0)) {
+        return false;
+    }
+    var half = 0;
+    while (x > half) {
+        half = half * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    return x === half || x === Math.floor(half / 10);
 };
