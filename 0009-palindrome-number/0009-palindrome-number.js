c47a74a4035d007af065f458906c3237265f4bc7
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-11 15:43:09 +0900

    Time: 128 ms (89.27%), Space: 50.9 MB (65.8%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,13 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const MinNum = -2147483648; //-2^31 값
-    const MaxNum = 2147483647;  //2^31-1 값
-    
-    if(x < MinNum || x > MaxNum) {
-        return false;
-    }
-    
-    const reverse =  x.toString().split("").reverse().join("");
-    return x === Number(reverse);
+    var s = x.toString();
+    var t = s.split("").reverse().join("");
+    return s === t;
 };
