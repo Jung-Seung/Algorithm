bb1963440953a0fc5b252ac786627e71dc7ceaca
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-12 23:44:53 +0900

    Time: 87 ms (55.21%), Space: 47.6 MB (29.32%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -3,44 +3,29 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function (s, p) {
-    const rows = s.length;
-    const columns = p.length;
-    /// Base conditions
-    if (rows == 0 && columns == 0) {
-        return true;
+var isMatch = function(s, p) {
+    const m = s.length, n = p.length;
+    const dp = new Array(m+1).fill().map(() => new Array(n+1).fill(false));
+    dp[0][0] = true; // empty pattern matches empty string
+
+    // initialize first row (empty string)
+    for (let j = 1; j <= n; j++) {
+        if (p[j-1] === '*')
+        dp[0][j] = dp[0][j-2];
     }
-    if (columns == 0) {
-        return false;
-    }
-    // DP array
-    const dp = Array.from({ length: s.length + 1 }, () => [false]);
-    // Empty string and empty pattern are a match
-    dp[0][0] = true;
-    // Deals with patterns with *
-    for (let i = 1; i < columns + 1; i++) {
-        if (p[i - 1] === '*') {
-            dp[0][i] = dp[0][i - 2];
-        }
-        else {
-            dp[0][i] = false;
-        };
-    }
-    // For remaining characters
-    for (let i = 1; i < rows + 1; i++) {
-        for (let j = 1; j < columns + 1; j++) {
-            if (p[j - 1] === '*') {
-                if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
-                    dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i][j - 2];
-                }
-            } else if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = false;
+
+    // fill in remaining cells
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+        if (s[i-1] === p[j-1] || p[j-1] === '.') {
+            dp[i][j] = dp[i-1][j-1];
+        } else if (p[j-1] === '*') {
+            dp[i][j] = dp[i][j-2]; // zero occurrences
+            if (s[i-1] === p[j-2] || p[j-2] === '.') {
+            dp[i][j] = dp[i][j] || dp[i-1][j]; // one or more occurrences
             }
         }
+        }
     }
-    return dp[rows][columns];
-};
+    return dp[m][n];
+}
