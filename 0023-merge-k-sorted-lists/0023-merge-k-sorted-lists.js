68e9f4edbfedf3d8c3a27ba3dad31f73b1c2e435
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-09-27 02:30:37 +0900

    Time: 112 ms (46.4%), Space: 50.6 MB (14.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,26 +9,28 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    if (!l1) {
-        return l2;
-    }
-    if (!l2) {
-        return l1;
-    }
-    if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
-    }
-};
+const mergeKLists = function(lists) {
+  const queue = new MinPriorityQueue({ priority: x => x.val })
 
-var mergeKLists = function(lists) {
-    let ans = null;
-    for (let i = 0; i < lists.length; i++) {
-        ans = mergeTwoLists(ans, lists[i]);
+  for (const head of lists) {
+    if (head) {
+      queue.enqueue(head)
     }
-    return ans;
-};
+  }
+
+  let result = new ListNode()
+  const head = result
+
+  while (!queue.isEmpty()) {
+    const { val, next } = queue.dequeue().element
+
+    result.next = new ListNode(val)
+    result = result.next
+
+    if (next) {
+      queue.enqueue(next)
+    }
+  }
+
+  return head.next
+}
