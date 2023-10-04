b55c7d89becfa4a50b190a7756be16764d644526
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-05 00:53:35 +0900

    Time: 72 ms (32.78%), Space: 44.9 MB (28.8%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,18 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0; // 빈 배열이면 0을 반환
-
-    let result = 1; // 배열의 첫 번째 요소는 이미 고유한 요소이므로 result를 1로 초기화
-
-    for (let i = 1; i < nums.length; i++) {
-        // 현재 요소와 이전 요소가 다른 경우, 중복이 아니므로 result를 1 증가시키고
-        // nums[result]에 현재 요소를 복사하여 고유한 요소로 처리
-        if (nums[i] !== nums[i - 1]) {
-            nums[result] = nums[i];
-            result++;
-        }
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
     }
-
-    return result; // 고유한 요소의 개수 result를 반환
-};
+  }
+  return i + 1;
+}
