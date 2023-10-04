bc634f3bac5d2c476991470c00c57902c13873d8
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-04 18:53:39 +0900

    Time: 68 ms (51.34%), Space: 47.1 MB (6.43%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    let list = new Array();
+    
+    while (head !== null) {
+        list.push(head.val);
+        head = head.next;
+    }
+
+    if (list.length === 0) return null;
+
+    for (let i = 0; i < list.length; i += k) {
+        let temp = list.slice(i, i+k);
+        if (temp.length < k) continue;
+        for (let j = 0; j < k; j++) {
+            list[i+j] = temp[k-j-1];
+        }
+    }
+
+    let result = new ListNode(list.pop());
+    while (list.length > 0) {
+        result = new ListNode(list.pop(), result);
+    }
+
+    return result;
+};
