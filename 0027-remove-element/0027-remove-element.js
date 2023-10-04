4dfc1b1f80d79d6ec58b0df2c3769d2864f73178
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-05 00:58:20 +0900

    Time: 54 ms (43.24%), Space: 42.4 MB (10.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -3,16 +3,15 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    let r = 0; // 유효한 요소의 개수를 나타내는 변수 r을 0으로 초기화
-
+function removeElement(nums, val) {
+    // Counter for keeping track of elements other than val
+    let count = 0;
+    // Loop through all the elements of the array
     for (let i = 0; i < nums.length; i++) {
-        // 현재 요소가 val과 같지 않으면 nums[r]에 복사하여 유효한 요소로 처리
+        // If the element is not val
         if (nums[i] !== val) {
-            nums[r] = nums[i];
-            r++;
+            nums[count++] = nums[i];
         }
     }
-
-    return r; // 유효한 요소의 개수 r을 반환
-};
+    return count;
+}
