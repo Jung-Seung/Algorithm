47b019ba8eb86b63d1e8eed84372c3f009b9fdc5
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-09 21:32:02 +0900

    Time: 55 ms (39.86%), Space: 41.9 MB (96.11%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,57 +4,33 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    // 시작 위치를 찾는 이진 검색 함수
-    function findStart(nums, target) {
-        let left = 0;
-        let right = nums.length - 1;
-        let start = -1;
-
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-
-            if (nums[mid] === target) {
-                start = mid;
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    let ans = [-1, -1];
+    let start = 0, end = nums.length - 1, mid;
+    // First occurrence
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (target == nums[mid]) {
+            ans[0] = mid;
+            end = mid - 1;
+        } else if (target < nums[mid]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
         }
-
-        return start;
     }
-
-    // 끝 위치를 찾는 이진 검색 함수
-    function findEnd(nums, target) {
-        let left = 0;
-        let right = nums.length - 1;
-        let end = -1;
-
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-
-            if (nums[mid] === target) {
-                end = mid;
-                left = mid + 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    // Last occurrence
+    start = 0;
+    end = nums.length - 1;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (target == nums[mid]) {
+            ans[1] = mid;
+            start = mid + 1;
+        } else if (target < nums[mid]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
         }
-
-        return end;
     }
-
-    const start = findStart(nums, target);
-    
-    // 시작 위치를 찾지 못한 경우
-    if (start === -1) {
-        return [-1, -1];
-    }
-    
-    const end = findEnd(nums, target);
-    return [start, end];
+    return ans;
 };
