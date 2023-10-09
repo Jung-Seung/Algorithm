1897adf21ff193da2efe41467ae3b0ac2446218f
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-09 20:45:45 +0900

    Time: 56 ms (34.02%), Space: 42.5 MB (12.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,36 +4,26 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
+    let start = 0, end = nums.length - 1;
+    let mid = Math.floor((start + end) / 2);
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (target === nums[mid]) {
             return mid;
         }
-
-        if (nums[left] <= nums[mid]) {
-            // 왼쪽 절반이 정렬되어 있는 경우
-            if (nums[left] <= target && target < nums[mid]) {
-                // 타겟이 왼쪽 절반에 속하는 경우
-                right = mid - 1;
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && nums[mid] >= target) {
+                end = mid - 1;
             } else {
-                // 타겟이 오른쪽 절반에 속하는 경우
-                left = mid + 1;
+                start = mid + 1;
             }
         } else {
-            // 오른쪽 절반이 정렬되어 있는 경우
-            if (nums[mid] < target && target <= nums[right]) {
-                // 타겟이 오른쪽 절반에 속하는 경우
-                left = mid + 1;
+            if (nums[end] >= target && nums[mid] <= target) {
+                start = mid + 1;
             } else {
-                // 타겟이 왼쪽 절반에 속하는 경우
-                right = mid - 1;
+                end = mid - 1;
             }
         }
     }
-
-    return -1; // 타겟을 찾지 못한 경우
-};
+    return -1;
+}
