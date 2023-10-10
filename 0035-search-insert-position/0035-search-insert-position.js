e69b8b7b254bd080399c6082fcaa8d9713921dba
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-10 18:07:00 +0900

    Time: 50 ms (65.7%), Space: 42.3 MB (34.87%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,21 +4,21 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-
+    let start = 0, end = nums.length - 1;
+    let ans = nums.length; // Default answer when target is greater than all elements
+    
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        
         if (nums[mid] === target) {
-            return mid; // target을 찾은 경우 해당 인덱스 반환
+            return mid;
         } else if (nums[mid] < target) {
-            left = mid + 1; // 중간 값보다 큰 경우 오른쪽 부분 탐색
+            start = mid + 1;
         } else {
-            right = mid - 1; // 중간 값보다 작은 경우 왼쪽 부분 탐색
+            ans = mid; // Update the answer to the current index
+            end = mid - 1;
         }
     }
-
-    // while 루프를 빠져나왔을 때 left가 삽입 위치가 됨
-    return left;
+    
+    return ans;
 };
