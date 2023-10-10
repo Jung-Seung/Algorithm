7ae9e355fb549a450e18addeae4f89bf4ad79f8e
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-11 01:50:17 +0900

    Time: 95 ms (61.34%), Space: 42.4 MB (92.13%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.js
+++ 0037-sudoku-solver/0037-sudoku-solver.js
@@ -2,47 +2,46 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
-var solveSudoku = function(board) {
-    solve(board);
-};
+function solveSudoku(board) {
+  const n = board.length;
+  dfs(board, n);
+}
 
-var solve = function(board) {
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            if (board[row][col] === ".") {
-                for (let num = 1; num <= 9; num++) {
-                    const numStr = num.toString();
-                    if (isValidMove(board, row, col, numStr)) {
-                        board[row][col] = numStr;
-                        if (solve(board)) {
-                            return true;
-                        }
-                        board[row][col] = ".";
-                    }
-                }
-                return false;
-            }
+function dfs(board, n) {
+  // for every cell in the sudoku
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      // if its empty
+      if (board[row][col] !== '.') continue;
+      // try every number 1-9
+      for (let i = 1; i <= 9; i++) {
+        const c = i.toString();
+        // if that number is valid
+        if (isValid(board, row, col, n, c)) {
+          board[row][col] = c;
+          // continue search for that board, ret true if solution is reached
+          if (dfs(board, n)) return true;
         }
+      }
+      // solution wasnt found for any num 1-9 here, must be a dead end...
+      // set the current cell back to empty
+      board[row][col] = '.';
+      // ret false to signal dead end 
+      return false;
     }
-    return true; // 스도쿠 퍼즐 완성
-};
+  }
+  // all cells filled, must be a solution
+  return true;
+}
 
-var isValidMove = function(board, row, col, num){
-    for (let i = 0; i < 9; i++) {
-        if (board[i][col] === num || board[row][i] === num) {
-            return false; // 같은 열 또는 같은 행에 중복된 숫자가 있음
-        }
-    }
-    
-    const startRow = Math.floor(row / 3) * 3;
-    const startCol = Math.floor(col / 3) * 3;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[startRow + i][startCol + j] === num) {
-                return false; // 같은 3x3 서브 그리드에 중복된 숫자가 있음
-            }
-        }
-    }
-    
-    return true;
-};
+function isValid(board, row, col, n, c) {
+  const blockRow = Math.floor(row / 3) * 3;
+  const blockCol = Math.floor(col / 3) * 3;
+  for (let i = 0; i < n; i++) {
+    if (board[row][i] === c || board[i][col] === c) return false;
+    const curRow = blockRow +  Math.floor(i / 3);
+    const curCol = blockCol +  Math.floor(i % 3);
+    if (board[curRow][curCol] === c) return false;
+  }
+  return true;
+}
