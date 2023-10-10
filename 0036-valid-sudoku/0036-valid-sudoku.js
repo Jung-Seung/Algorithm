e489e77ccd3dcf94febcf7ebad33cbcc6d583611
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-11 00:30:46 +0900

    Time: 62 ms (77.85%), Space: 45.3 MB (54.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,51 +3,28 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    // 행 검사
-    for (let row = 0; row < 9; row++) {
-        const rowSet = new Set();
-        for (let col = 0; col < 9; col++) {
-            const cell = board[row][col];
-            if (cell !== '.' && rowSet.has(cell)) {
-                return false;
+    
+    for(let i =0; i<board.length;i++){
+        let rowMap = {}
+        let colMap = {}
+        let boxMap = {}
+        for(let j=0; j<board[i].length;j++){
+
+            let box = board[3* Math.floor(i/3) +Math.floor(j/3)][3*(i%3)+(j%3)]
+            
+            if(board[i][j] != '.'){
+                if(rowMap[board[i][j]]) return false;
+                rowMap[board[i][j]] = 1;
             }
-            if (cell !== '.') {
-                rowSet.add(cell);
+            if(board[j][i] != '.') {
+                if(colMap[board[j][i]]) return false;
+                colMap[board[j][i]] = 1;
+            }
+            if(box!= '.'){
+                if(boxMap[box]) return false;
+                boxMap[box] = 1;
             }
         }
     }
-
-    // 열 검사
-    for (let col = 0; col < 9; col++) {
-        const colSet = new Set();
-        for (let row = 0; row < 9; row++) {
-            const cell = board[row][col];
-            if (cell !== '.' && colSet.has(cell)) {
-                return false;
-            }
-            if (cell !== '.') {
-                colSet.add(cell);
-            }
-        }
-    }
-
-    // 3x3 서브 그리드 검사
-    for (let block = 0; block < 9; block++) {
-        const blockSet = new Set();
-        const startRow = Math.floor(block / 3) * 3;
-        const startCol = (block % 3) * 3;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const cell = board[startRow + i][startCol + j];
-                if (cell !== '.' && blockSet.has(cell)) {
-                    return false;
-                }
-                if (cell !== '.') {
-                    blockSet.add(cell);
-                }
-            }
-        }
-    }
-
     return true;
 };
