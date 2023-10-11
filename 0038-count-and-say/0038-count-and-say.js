314cd2328d99326ec0bd2d0764a1b9e87c0beed9
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-11 23:05:28 +0900

    Time: 58 ms (53.23%), Space: 44.1 MB (84.41%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.js
+++ 0038-count-and-say/0038-count-and-say.js
@@ -3,28 +3,9 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    if (n === 1) return "1";
-
-    let result = "1";
-    for (let i = 2; i <= n; i++) {
-        result = say(result);
-    }
-
-    return result;
-};
-
-var say = function(str){
-    let result = "";
-    let count = 1;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === str[i + 1]) {
-            count++;
-        } else {
-            result += count + str[i];
-            count = 1;
-        }
-    }
-
-    return result;
+    if (n === 1) return '1';
+    
+    return countAndSay(n-1)
+        .match(/1+|2+|3+/g)
+        .reduce((acc, nums) => acc += `${nums.length}${nums[0]}`, '')
 };
