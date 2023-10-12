210c75df9134129882d398e5c2aad69fe3b36ab9
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-13 01:26:44 +0900

    Time: 82 ms (50.12%), Space: 61.6 MB (11.76%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -2,30 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {
-    const n = nums.length;
-
-    // 1. 음수와 0을 n+1로 변경하여 처리합니다.
-    for (let i = 0; i < n; i++) {
-        if (nums[i] <= 0) {
-            nums[i] = n + 1;
-        }
+var firstMissingPositive = function(nums) {    
+    let m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        m.set(nums[i], 1);
     }
-
-    // 2. 1부터 n까지의 숫자가 배열에 존재하는 경우, 해당 위치의 값을 음수로 변경합니다.
-    for (let i = 0; i < n; i++) {
-        const num = Math.abs(nums[i]);
-        if (num <= n) {
-            nums[num - 1] = -Math.abs(nums[num - 1]);
-        }
+    for (let i = 1; i <= nums.length; i++) {
+        if (!m.has(i)) return i;
     }
-
-    // 3. 양수인 첫 번째 값의 위치가 누락된 최소 양의 정수입니다.
-    for (let i = 0; i < n; i++) {
-        if (nums[i] > 0) {
-            return i + 1;
-        }
-    }
-
-    return n + 1; // 1부터 n까지의 숫자가 모두 존재하는 경우
-};
+    return nums.length + 1; // the array is [1,2,...,n]
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    /*
+    The worst case (the first missing positive being the greatest) is
+    when the array is [1,2..,n]. Therefore, in all other cases except this case, 
+    the first missing positive number is less than or equal to n (nums.length).
+    */
+}
