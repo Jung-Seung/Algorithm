4d381bab1b9c2d8e9ffae78d1627e70c686ac2d7
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-17 23:57:20 +0900

    Time: 59 ms (54.34%), Space: 43.9 MB (67.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -2,26 +2,45 @@
  * @param {number[]} nums
  * @return {number}
  */
- /*
-    1. 주어진 배열을 순회하면서 각 위치에서의 최대 점프 범위를 계산합니다.
-    2. 현재 위치에서의 최대 점프 범위가 현재 인덱스를 벗어나면 점프 횟수를 증가시키고 최대 점프 범위를 업데이트합니다.
-    3. 배열의 끝까지 이 과정을 반복하여 최소 점프 수를 찾습니다.
- */
 var jump = function(nums) {
-    let n = nums.length;
-    if (n < 2) return 0;
-    
-    let maxPosition = nums[0];
-    let maxSteps = nums[0];
-    let jumps = 1;
-    
-    for (let i = 1; i < n; i++) {
-        if (maxSteps < i) {
-            jumps++;
-            maxSteps = maxPosition;
-        }
-        maxPosition = Math.max(maxPosition, nums[i] + i);
+
+    const size = nums.length;
+
+    // destination is last index
+    let destination = size-1;
+
+    let curCoverage = 0, lastJumpIdx = 0;
+
+    // counter of jump
+    let timesOfJump = 0;
+
+    // Quick response if start index == destination index == 0
+    if( size == 1 ){
+        return 0;
     }
+
+
+    // Greedy stragegy: extend coverage as long as possible with lazp jump
+    for( let i = 0 ; i < size ; i++){
+
+        // extend coverage
+        curCoverage = Math.max(curCoverage, i + nums[i] );
+
+        // forced to jump (by lazy jump) to extend coverage
+        if( i == lastJumpIdx ){
+
+            lastJumpIdx = curCoverage;
+
+            timesOfJump++;
+
+            // check if we reached destination already
+            if( curCoverage >= destination){
+                return timesOfJump;
+            }
+        }
+    }
+
+    return timesOfJump;
     
-    return jumps;
+
 };
