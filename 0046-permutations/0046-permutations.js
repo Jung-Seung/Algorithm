8a6a6450d31e13bc112aa6dde08d1fa9466685d6
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-18 01:03:16 +0900

    Time: 63 ms (55.84%), Space: 45.2 MB (22.81%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -2,28 +2,27 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- /*
-    1. backtrack 함수를 정의하여 현재 순열과 남은 요소를 추적합니다.
-    2. 재귀적으로 모든 가능한 순열을 생성합니다.
-    3. 기저 사례에 도달하면 현재 순열을 결과에 추가합니다.
-    4. 모든 요소에 대해 이 과정을 반복하여 모든 가능한 순열을 찾습니다.
- */
-var permute = function(nums) {
-     const result = [];
-    
-    const backtrack = (current, remaining) => {
-        if (remaining.length === 0) {
-            result.push(current);
-            return;
-        }
-        
-        for (let i = 0; i < remaining.length; i++) {
-            const newCurrent = [...current, remaining[i]];
-            const newRemaining = remaining.filter((_, index) => index !== i);
-            backtrack(newCurrent, newRemaining);
-        }
-    };
-    
-    backtrack([], nums);
-    return result;
-};
+var permute = function(letters) {
+    let res = [];
+    dfs(letters, [], Array(letters.length).fill(false), res);
+    return res;
+}
+
+function dfs(letters, path, used, res) {
+    if (path.length == letters.length) {
+        // make a deep copy since otherwise we'd be append the same list over and over
+        res.push(Array.from(path));
+        return;
+    }
+    for (let i = 0; i < letters.length; i++) {
+        // skip used letters
+        if (used[i]) continue;
+        // add letter to permutation, mark letter as used
+        path.push(letters[i]);
+        used[i] = true;
+        dfs(letters, path, used, res);
+        // remove letter from permutation, mark letter as unused
+        path.pop();
+        used[i] = false;
+    }
+}
