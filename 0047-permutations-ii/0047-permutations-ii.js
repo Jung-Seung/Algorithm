f5030d66d8b4c8d4207db5869177bf925bb9c517
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-18 19:15:40 +0900

    Time: 61 ms (87.29%), Space: 44.4 MB (89.58%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.js
+++ 0047-permutations-ii/0047-permutations-ii.js
@@ -2,30 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- /*
-    1. 배열을 먼저 정렬하여 중복된 숫자가 인접하도록 합니다.
-    2. backtrack 함수는 현재 순열과 남은 숫자의 배열을 매개변수로 사용합니다.
-    3. 만약 remaining이 빈 배열이면, 현재 순열을 결과에 추가합니다.
-    4. 반복문을 통해 남은 숫자 배열을 순회하면서 중복된 수를 건너뜁니다.
-    5. 현재 숫자를 추가하고, 재귀적으로 순열을 찾습니다. 재귀 호출이 끝나면 현재 숫자를 제거합니다.
- */
 var permuteUnique = function(nums) {
-    const result = [];
-    nums.sort((a, b) => a - b);
-    const backtrack = (current, remaining) => {
-        if (remaining.length === 0) {
-            result.push(current.slice());
-            return;
+    const res = [];
+    const countMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const curVal = nums[i];
+
+        if (!countMap[curVal]) {
+            countMap[curVal] = 1;
+        } else {
+            countMap[curVal] += 1;
         }
-        for (let i = 0; i < remaining.length; i++) {
-            if (i > 0 && remaining[i] === remaining[i - 1]) {
-                continue;
-            }
-            current.push(remaining[i]);
-            backtrack(current, remaining.slice(0, i).concat(remaining.slice(i + 1)));
-            current.pop();
-        }
-    };
-    backtrack([], nums);
-    return result;
+    } 
+
+    dfs(nums, [], countMap, res);
+    return res;
 };
+
+function dfs(nums, path, countMap, res) {
+    if (path.length === nums.length) {
+        return res.push([...path]);
+    }
+
+    for (const num in countMap) {
+        const numCount = countMap[num];
+        
+        if (numCount === 0) continue;
+
+        path.push(num);
+        countMap[num] -= 1;
+
+        dfs(nums, path, countMap, res);
+
+        path.pop();
+        countMap[num] += 1;
+    }
+}
