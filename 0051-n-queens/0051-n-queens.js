4f1a0ef2a64c4a8455603de6e3b771711fcaf4d3
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-20 02:45:56 +0900

    Time: 65 ms (65.33%), Space: 45.2 MB (50.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.js
+++ 0051-n-queens/0051-n-queens.js
@@ -2,52 +2,52 @@
  * @param {number} n
  * @return {string[][]}
  */
-/*
-    1. solveNQueens 함수는 N-Queens 문제를 해결하는 함수입니다. n은 보드 크기입니다.
-    2. results 배열은 모든 가능한 해결책을 저장할 곳입니다.
-    3. board 배열은 현재의 보드 상태를 저장합니다. 처음에는 모든 위치가 비어 있습니다.
-    4. isSafe 함수는 주어진 행과 열에 퀸을 배치할 수 있는지를 확인합니다. 다른 퀸과 겹치지 않도록 검사합니다.
-    5. solve 함수는 백트래킹을 수행합니다. 각 행에 대해 가능한 열에 퀸을 배치하고, 이를 재귀적으로 반복합니다.
-    6. solve 함수를 호출하여 가능한 모든 배치를 시도하고, 유효한 경우 결과에 추가합니다.
-*/
-var solveNQueens = function(n) {
-    const results = [];
-    const board = Array(n).fill().map(() => Array(n).fill('.'));
-    const isSafe = (row, col) => {
-        // 현재 행 위의 열을 확인합니다.
-        for (let i = 0; i < row; i++) {
-            if (board[i][col] === 'Q') {
-                return false;
-            }
+var solveNQueens = function(n, start = 0 , mat) {
+    var mat = [...new Array(n)].map( ele => new Array(n).fill(".") );
+    var len = mat.length
+    var result = [];
+    var count = 0
+    helper(0, mat);
+    return result
+    
+    function helper(row , mat ){
+        if(row == n){
+            result.push([...mat].map(ele => ele.join("")));
+            return
         }
-        // 왼쪽 위 대각선을 확인합니다.
-        for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] === 'Q') {
-                return false;
-            }
-        }
-        // 오른쪽 위 대각선을 확인합니다.
-        for (let i = row, j = col; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] === 'Q') {
-                return false;
-            }
-        }
-        return true;
-    };
-    const solve = (row) => {
-        if (row === n) {
-            // 모든 행이 채워졌다면 현재 보드를 결과에 추가합니다.
-            results.push(board.map(row => row.join('')));
-            return;
-        }
-        for (let col = 0; col < n; col++) {
-            if (isSafe(row, col)) {
-                board[row][col] = 'Q'; // 퀸을 배치합니다.
-                solve(row + 1); // 다음 행으로 넘어갑니다.
-                board[row][col] = '.'; // 백트래킹합니다.
-            }
-        }
-    };
-    solve(0); // 첫 번째 행부터 시작합니다.
-    return results;
+        for(let j=0; j<len; j++){
+            if(checkDigonal(row , j ,mat, n) ){
+                mat[row][j] = 'Q';
+                helper(row + 1 , mat)
+                mat[row][j] = '.';
+            }   
+        } 
+    }
 };
+
+function checkDigonal(i , j ,mat, len){
+    
+    var x = i-1;
+    var y = j-1;
+    
+        while(x>= 0 && x < len && y>=0 && y<len){
+            if(mat[x][y] == 'Q')return false
+            x = x-1;
+            y = y-1;
+        }
+        x = i-1;
+        y = j+1;
+    
+        while(x>= 0 && x < len && y>=0 && y<len){
+            if(mat[x][y] == 'Q')return false
+            x = x-1;
+            y = y+1;
+        }
+        x = i-1;
+        y = j
+        while(x>= 0 && x < len && y>=0 && y<len){
+            if(mat[x][y] == 'Q')return false
+            x--
+        }
+    return true
+}
