aa0d5311a24872e2aaeffa4175f8f880508a6b81
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-25 12:40:19 +0900

    Time: 74 ms (16.05%), Space: 44.1 MB (70.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -3,29 +3,26 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-var insert = function(intervals, newInterval) {
-    const result = [];
-    let i = 0;
-
-    // newInterval이 현재 간격과 겹치지 않는 부분까지 result에 추가합니다.
-    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-        result.push(intervals[i]);
-        i++;
+var insert = function (intervals, newInterval) {
+  let [start, end] = newInterval;
+  let left = [];
+  let right = [];
+  
+  for (const interval of intervals) {
+    const [first, last] = interval;
+	
+	// current interval is smaller than newInterval
+    if (last < start) left.push(interval);
+	
+	// current interval is larger than newInterval
+    else if (first > end) right.push(interval);
+	
+	// there is a overlap
+    else {
+      start = Math.min(start, first);
+      end = Math.max(end, last);
     }
-
-    // newInterval이 현재 간격과 겹치는 부분을 병합합니다.
-    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-        i++;
-    }
-    result.push(newInterval);
-
-    // 나머지 간격들을 result에 추가합니다.
-    while (i < intervals.length) {
-        result.push(intervals[i]);
-        i++;
-    }
-
-    return result;
+  }
+  
+  return [...left, [start, end], ...right]; 
 };
