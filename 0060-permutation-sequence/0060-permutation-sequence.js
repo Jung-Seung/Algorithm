270790058bfdcdec0196d028021ee7c1d32571cb
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-27 02:26:00 +0900

    Time: 42 ms (97.37%), Space: 42 MB (60.53%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.js
+++ 0060-permutation-sequence/0060-permutation-sequence.js
@@ -4,33 +4,16 @@
  * @return {string}
  */
 var getPermutation = function(n, k) {
-    // 팩토리얼 값을 저장할 배열
     let factorial = [1];
-    // 1부터 n까지의 수를 저장할 배열
-    let nums = [];
-    // 결과를 저장할 변수
-    let result = "";
+    for (let i=1;i<=n;i++) factorial[i]= i * factorial[i-1];
 
-    // 팩토리얼 사전 계산
-    for (let i = 1; i <= n; i++) {
-        factorial[i] = factorial[i - 1] * i;
-        nums.push(i);
+    let nums = Array.from({length: n}, (v, i) => i+1);
+    let res = "";
+    for (let i=n;i>0;i--) {
+        index = Math.ceil(k / factorial[i - 1]); // decide to use which permutation set
+        res+=nums[index - 1];
+        nums.splice(index - 1, 1);
+        k -= (factorial[i-1] * (index - 1));
     }
-
-    // k를 0부터 시작하도록 1 감소시킴
-    k--;
-
-    // k번째 순열 계산
-    for (let i = 1; i <= n; i++) {
-        // 현재 인덱스 계산
-        let index = Math.floor(k / factorial[n - i]);
-        // 결과에 해당하는 숫자 추가
-        result += nums[index];
-        // 사용된 숫자는 배열에서 제거
-        nums.splice(index, 1);
-        // 다음 순열을 위해 k 갱신
-        k -= index * factorial[n - i];
-    }
-
-    return result;
+    return res;
 };
