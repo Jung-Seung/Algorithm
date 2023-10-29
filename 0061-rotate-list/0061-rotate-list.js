4e169d1cbcecfa66ecd8752a3cb2c9c00c331c02
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-30 02:25:36 +0900

    Time: 54 ms (83.04%), Space: 44.4 MB (18.77%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.js
+++ 0061-rotate-list/0061-rotate-list.js
@@ -10,32 +10,50 @@
  * @param {number} k
  * @return {ListNode}
  */
-var rotateRight = function(head, k) {
-    // 예외 처리: 헤드가 없거나 회전할 횟수가 0인 경우
-    if (!head || k === 0) return head;
+/* 
+Algorithm Explanation: (Image attached in comments)
+1.  We count the total number of nodes in the list using count variable,
+2.  Now the total rotations we have to do can be restricted to k= k % count.
+3.  Now we move a pointer from head equal to k positions. This is necessary 
+for the next interesting logic
+4. Now since our ptr is k steps ahead then it will tell us the kth position from end
+i.e we iterate until ptr.next !==null and we move our prev pointer. Once ptr.next===null
+i.e ptr reaches the last node, out prev pointer will be exactly at the kth position from end
+Here kth position from end is nothing but the number of k rotations
+5. Now simply break the list after prev node. Last node i.e ptr will now point to head
+i.e ptr.next=head, next node of prev becomes our new head and finally prev becomes our 
+last node so prev.next=null
+*/
+var rotateRight = function (head, k) {
+	if (!head) return head;
+	let count = 0,
+		ptr = head;
 
-    // 연결 리스트의 길이 계산
-    let length = 1;
-    let current = head;
-    while (current.next) {
-        current = current.next;
-        length++;
-    }
+	//Step 1 of the algo, count list nodes
+	while (ptr) {
+		count++;
+		ptr = ptr.next;
+	}
 
-    // 연결 리스트를 순환 리스트로 만듦
-    current.next = head;
+	//Ste 2: Number of rotations are now restricted within limit
+	k = k % count;
+	let prev = head;
+	ptr = head;
 
-    // k 값이 length보다 큰 경우를 대비하여 나머지 연산
-    k = k % length;
+	//Step 3: Moving one pointer k positions ahead
+	while (k--) {
+		ptr = ptr.next;
+	}
 
-    // 오른쪽으로 회전하는 동안 현재 노드를 이동시킴
-    for (let i = 0; i < length - k; i++) {
-        current = current.next;
-    }
+	//Step 4: The actual magic, explained above
+	while (ptr.next) {
+		prev = prev.next;
+		ptr = ptr.next;
+	}
 
-    // 새로운 헤드 노드 설정 및 기존의 연결 리스트 끊어줌
-    let newHead = current.next;
-    current.next = null;
-
-    return newHead;
+	//Step 5: Simply modifying the head and last node
+	ptr.next = head;
+	head = prev.next;
+	prev.next = null;
+	return head;
 };
