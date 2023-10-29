01a7d934df324675445c45c375b8bd232dff4543
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-10-30 03:01:31 +0900

    Time: 53 ms (50.26%), Space: 42.1 MB (55.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,24 +4,39 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    // Create a 2D DP array filled with zeros
-    let dp = new Array(m).fill().map(() => new Array(n).fill(0));
+    // Create a memoization table to store computed results
+    let memo = new Array(m).fill(null).map(() => new Array(n).fill(-1));
     
-    // Initialize the rightmost column and bottom row to 1
-    for (let i = 0; i < m; i++) {
-        dp[i][n-1] = 1;
-    }
-    for (let j = 0; j < n; j++) {
-        dp[m-1][j] = 1;
-    }
-    
-    // Fill in the DP array bottom-up
-    for (let i = m - 2; i >= 0; i--) {
-        for (let j = n - 2; j >= 0; j--) {
-            dp[i][j] = dp[i+1][j] + dp[i][j+1];
-        }
+    // Call the recursive function to compute unique paths
+    return uniquePathsRecursive(0, 0, m, n, memo);
+};
+
+var uniquePathsRecursive = function(x, y, m, n, memo) {
+    // If we reach the destination (bottom-right corner), return 1
+    if (x === m - 1 && y === n - 1) {
+        return 1;
     }
     
-    // Return the result stored in the top-left corner
-    return dp[0][0];
+    // If we have already computed the result for this cell, return it from the memo table
+    if (memo[x][y] !== -1) {
+        return memo[x][y];
+    }
+    
+    // Calculate the number of unique paths by moving right and down
+    let rightPaths = 0;
+    let downPaths = 0;
+    
+    // Check if it's valid to move right
+    if (x < m - 1) {
+        rightPaths = uniquePathsRecursive(x + 1, y, m, n, memo);
+    }
+    
+    // Check if it's valid to move down
+    if (y < n - 1) {
+        downPaths = uniquePathsRecursive(x, y + 1, m, n, memo);
+    }
+    
+    // Store the result in the memo table and return it
+    memo[x][y] = rightPaths + downPaths;
+    return memo[x][y];
 };
