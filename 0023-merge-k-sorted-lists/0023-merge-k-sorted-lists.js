bd7386b747d8ef434bf7bcbb7a6af3e631890e0e
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-10 07:52:09 +0900

    Update 0023-merge-k-sorted-lists.js

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,28 +9,36 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-const mergeKLists = function(lists) {
+var mergeKLists = function(lists) {
+  // 우선순위 큐 생성
   const queue = new MinPriorityQueue({ priority: x => x.val })
 
+  // 리스트의 헤드를 우선순위 큐에 추가
   for (const head of lists) {
     if (head) {
       queue.enqueue(head)
     }
   }
 
+  // 결과를 저장할 빈 노드 생성
   let result = new ListNode()
   const head = result
 
+  // 큐가 비어있지 않은 동안 연산을 반복
   while (!queue.isEmpty()) {
+    // 큐에서 우선순위가 가장 높은 노드 추출
     const { val, next } = queue.dequeue().element
 
+    // 추출한 노드의 값을 결과 노드에 추가
     result.next = new ListNode(val)
     result = result.next
 
+    // 추출한 노드의 다음 노드가 존재하면 큐에 추가
     if (next) {
       queue.enqueue(next)
     }
   }
 
+  // 결과 노드의 다음 노드 반환
   return head.next
-}
+};
