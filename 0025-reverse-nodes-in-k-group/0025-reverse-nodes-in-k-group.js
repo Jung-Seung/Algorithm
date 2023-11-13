f92539e255ae8fb3febbf000316fdf1ac0dc4d82
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-14 01:24:14 +0900

    Update 0025-reverse-nodes-in-k-group.js

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -11,25 +11,33 @@
  * @return {ListNode}
  */
 var reverseKGroup = function(head, k) {
+    // 가장 처음의 더미 노드 생성
     var dummy = new ListNode(0);
-        dummy.next = head;
-        var prevGroupTail = dummy;
+    dummy.next = head;
+    var prevGroupTail = dummy;
 
-        while (head) {
-            var groupStart = head;
-            var groupEnd = getGroupEnd(head, k);
+    // head가 존재하는 동안 반복
+    while (head) {
+        // 현재 그룹의 시작점 할당
+        var groupStart = head;
+        // 현재 그룹의 끝점 계산
+        var groupEnd = getGroupEnd(head, k);
 
-            if (!groupEnd)
-                break;
+        // 그룹의 끝이 존재하지 않을 경우 반복 종료
+        if (!groupEnd)
+            break;
 
-            prevGroupTail.next = reverseList(groupStart, groupEnd.next);
-            prevGroupTail = groupStart;
-            head = prevGroupTail.next;
-        }
+        // 이전 그룹의 꼬리를 현재 그룹의 뒤집힌 리스트의 헤드로 설정
+        prevGroupTail.next = reverseList(groupStart, groupEnd.next);
+        prevGroupTail = groupStart;
+        head = prevGroupTail.next;
+    }
+    // 더미 노드의 다음 노드를 새로운 헤드 노드로 설정
     var newHead = dummy.next;
     return newHead;
 }
 
+// 그룹의 끝점을 찾는 함수
 var getGroupEnd = function(head, k) {
     while (head && k > 1) {
         head = head.next;
@@ -38,6 +46,7 @@ var getGroupEnd = function(head, k) {
     return head;
 }
 
+// 노드를 뒤집는 함수
 var reverseList = function(head, stop) {
     var prev = stop;
     while (head !== stop) {
