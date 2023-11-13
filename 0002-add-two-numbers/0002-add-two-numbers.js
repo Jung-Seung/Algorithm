983e138bfef5283a3b3c4b7f924e4cee2030ae98
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-14 01:47:45 +0900

    Update 0002-add-two-numbers.js

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -12,10 +12,15 @@
  */
 var addTwoNumbers = function(l1, l2) {
     const iter = (n1, n2, rest = 0) => {
+        // 두 연결 리스트가 끝에 도달하고, 올림 값이 없는 경우 null을 반환합니다.
         if (!n1 && !n2 && !rest) return null;
+        // 현재 자리의 숫자를 계산합니다.
         const newVal = (n1?.val || 0) + (n2?.val || 0) + rest;
+        // 다음 자리의 노드를 재귀적으로 계산합니다.
         const nextNode = iter(n1?.next, n2?.next, Math.floor(newVal / 10));
+        // 현재 노드를 생성하고 반환합니다.
         return new ListNode(newVal % 10, nextNode);
     }
+    // 초기 호출로 결과를 반환합니다.
     return iter(l1, l2);
 };
