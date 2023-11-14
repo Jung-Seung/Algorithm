9747e4a4e045b4a22e65a2b9e3846c9dd4b6a316
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-15 02:25:42 +0800

    Update 0008-string-to-integer-atoi.js

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -3,24 +3,36 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    s = s.trim();  // Remove leading and trailing whitespace
+    s = s.trim();  // 선행 및 후행 공백 제거
+
     if (s.length === 0) {
-        return 0;  // Handle empty string case
+        return 0;  // 빈 문자열 처리
     }
-    let num = 0;
-    let i = 0;
-    let sign = 1;  // 1 for positive, -1 for negative
+
+    let num = 0;  // 결과를 저장할 변수 초기화
+    let i = 0;    // 문자열 인덱스 초기화
+    let sign = 1;  // 부호 저장, 양수면 1, 음수면 -1
+
+    // 부호 확인
     if (s[i] === '+') {
         i++;
     } else if (s[i] === '-') {
         i++;
         sign = -1;
     }
+
+    // 숫자 추출
     while (i < s.length && /^\d$/.test(s[i])) {
         num = num * 10 + parseInt(s[i]);
         i++;
     }
+
+    // 부호 적용
     num *= sign;
-    num = Math.max(Math.min(num, Math.pow(2, 31) - 1), -Math.pow(2, 31));  // Check for integer overflow
+
+    // 정수 오버플로우 체크
+    num = Math.max(Math.min(num, Math.pow(2, 31) - 1), -Math.pow(2, 31));
+
+    // 최종 변환된 정수 반환
     return num;
-}
+};
