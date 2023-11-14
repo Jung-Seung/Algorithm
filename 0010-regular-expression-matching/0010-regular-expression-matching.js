2daf973754bdae6f92f0600339076ccfc40f17bf
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-15 03:23:54 +0800

    Update 0010-regular-expression-matching.js

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -5,27 +5,31 @@
  */
 var isMatch = function(s, p) {
     const m = s.length, n = p.length;
+    
+    // DP 테이블 초기화
     const dp = new Array(m+1).fill().map(() => new Array(n+1).fill(false));
-    dp[0][0] = true; // empty pattern matches empty string
+    dp[0][0] = true; // 빈 패턴은 빈 문자열과 일치
 
-    // initialize first row (empty string)
+    // 첫 번째 행 초기화 (빈 문자열)
     for (let j = 1; j <= n; j++) {
-        if (p[j-1] === '*')
-        dp[0][j] = dp[0][j-2];
+        if (p[j-1] === '*') {
+            dp[0][j] = dp[0][j-2];
+        }
     }
 
-    // fill in remaining cells
+    // 나머지 셀 채우기
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
-        if (s[i-1] === p[j-1] || p[j-1] === '.') {
-            dp[i][j] = dp[i-1][j-1];
-        } else if (p[j-1] === '*') {
-            dp[i][j] = dp[i][j-2]; // zero occurrences
-            if (s[i-1] === p[j-2] || p[j-2] === '.') {
-            dp[i][j] = dp[i][j] || dp[i-1][j]; // one or more occurrences
+            if (s[i-1] === p[j-1] || p[j-1] === '.') {
+                dp[i][j] = dp[i-1][j-1];
+            } else if (p[j-1] === '*') {
+                dp[i][j] = dp[i][j-2]; // 0 회 이상의 발생
+                if (s[i-1] === p[j-2] || p[j-2] === '.') {
+                    dp[i][j] = dp[i][j] || dp[i-1][j]; // 1 회 이상의 발생
+                }
             }
         }
-        }
     }
+
     return dp[m][n];
-}
+};
