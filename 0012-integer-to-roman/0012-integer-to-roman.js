2b2a1869c69ff199016784f2e0daf2e810dcc6b5
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-16 22:21:56 +0900

    Update 0012-integer-to-roman.js

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -2,10 +2,17 @@
  * @param {number} num
  * @return {string}
  */
+// 정수를 로마 숫자로 변환하는 함수
 var intToRoman = function(num) {
+    // 각 자릿수별 로마 숫자를 저장한 배열들 정의
     const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
     const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
     const hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
     const ths = ["", "M", "MM", "MMM"];
-    return ths[Math.floor(num / 1000)] + hrns[Math.floor((num % 1000) / 100)] + tens[Math.floor((num % 100) / 10)] + ones[num % 10];
+
+    // 각 자릿수에 해당하는 로마 숫자를 결합하여 결과 반환
+    return ths[Math.floor(num / 1000)] +  // 천의 자리
+           hrns[Math.floor((num % 1000) / 100)] +  // 백의 자리
+           tens[Math.floor((num % 100) / 10)] +  // 십의 자리
+           ones[num % 10];  // 일의 자리
 };
