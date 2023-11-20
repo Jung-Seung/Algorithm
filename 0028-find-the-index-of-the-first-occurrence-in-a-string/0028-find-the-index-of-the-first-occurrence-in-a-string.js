174fffe47ee55602c90e9b8a233c4ea44310c1a8
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-21 00:42:34 +0900

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.js

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,10 +4,10 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let n = haystack.length;
-    let m = needle.length;
+    let n = haystack.length; // 전체 문자열의 길이
+    let m = needle.length;   // 부분 문자열의 길이
     
-    if (m === 0) return 0; // empty needle
+    if (m === 0) return 0;   // 부분 문자열이 비어있는 경우
     
     for (let i = 0; i <= n - m; i++) {
         let found = true;
@@ -17,7 +17,7 @@ var strStr = function(haystack, needle) {
                 break;
             }
         }
-        if (found) return i;
+        if (found) return i; // 부분 문자열 발견 시 시작 인덱스 반환
     }
-    return -1; // needle not found
-}
+    return -1; // 부분 문자열을 찾지 못한 경우
+};
