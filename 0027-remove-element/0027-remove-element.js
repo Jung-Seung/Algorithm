2fba34ca94f5ef3a8f7fb48a9aa7456a4b83d4ac
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-21 00:16:25 +0900

    Update 0027-remove-element.js

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,14 +4,16 @@
  * @return {number}
  */
 function removeElement(nums, val) {
-    // Counter for keeping track of elements other than val
+    // 특정 값과 일치하지 않는 요소를 저장할 변수 count 초기화
     let count = 0;
-    // Loop through all the elements of the array
+    // 배열의 모든 요소를 순회
     for (let i = 0; i < nums.length; i++) {
-        // If the element is not val
+        // 현재 요소가 특정 값과 일치하지 않을 경우
         if (nums[i] !== val) {
+            // count 위치에 현재 요소를 덮어씌우고 count를 증가
             nums[count++] = nums[i];
         }
     }
+    // 특정 값이 제거된 배열의 길이 반환
     return count;
 }
