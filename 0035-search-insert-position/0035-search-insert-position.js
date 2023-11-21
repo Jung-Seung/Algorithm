ee5fb94c0db0bcdcc38e12e6a2c9cd6d8583fbd7
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-22 01:20:00 +0900

    Update 0035-search-insert-position.js

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,21 +4,26 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+    // 시작과 끝 인덱스 초기화
     let start = 0, end = nums.length - 1;
-    let ans = nums.length; // Default answer when target is greater than all elements
-    
+    // 기본값 설정: target이 모든 요소보다 클 때 배열의 길이로 설정
+    let ans = nums.length;
+    // 이진 검색 수행
     while (start <= end) {
+        // 중간 인덱스 계산
         let mid = Math.floor((start + end) / 2);
-        
+        // target을 찾은 경우 해당 인덱스 반환
         if (nums[mid] === target) {
             return mid;
         } else if (nums[mid] < target) {
+            // 중간 값이 target보다 작으면 오른쪽 절반 탐색
             start = mid + 1;
         } else {
-            ans = mid; // Update the answer to the current index
+            // 중간 값이 target보다 크면 왼쪽 절반 탐색
+            ans = mid; // 현재 인덱스로 답을 갱신
             end = mid - 1;
         }
     }
-    
+    // 결과 반환
     return ans;
 };
