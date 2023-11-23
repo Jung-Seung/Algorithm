541b7da93313fbb453ab14f36ac9124b5c9da693
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-23 12:35:18 +0900

    Update 0041-first-missing-positive.js

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -2,20 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {    
+var firstMissingPositive = function(nums) {
+    // 정수를 저장할 맵 생성
     let m = new Map();
+
+    // 배열의 각 요소를 맵에 추가
     for (let i = 0; i < nums.length; i++) {
         m.set(nums[i], 1);
     }
+    // 1부터 배열 길이까지의 숫자 중 맵에 없는 첫 번째 숫자 반환
     for (let i = 1; i <= nums.length; i++) {
-        if (!m.has(i)) return i;
+        if (!m.has(i)) {
+            return i;
+        }
     }
-    return nums.length + 1; // the array is [1,2,...,n]
-    // Time Complexity: O(n)
-    // Space Complexity: O(n)
-    /*
-    The worst case (the first missing positive being the greatest) is
-    when the array is [1,2..,n]. Therefore, in all other cases except this case, 
-    the first missing positive number is less than or equal to n (nums.length).
-    */
-}
+    // 모든 숫자가 존재하면 배열 길이보다 1 큰 값을 반환
+    return nums.length + 1;
+};
+// Time Complexity: O(n)
+// Space Complexity: O(n)
