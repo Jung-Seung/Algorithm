755ee8c7241de0d2a244c192f3d19e5423768742
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-27 03:45:55 +0800

    Update 0049-group-anagrams.js

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,13 +2,19 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    let map = {}
 
+// 일련의 문자열을 받아 애너그램 단위로 그룹화하는 함수
+var groupAnagrams = function(strs) {
+    let map = {};  // 애너그램을 그룹화할 객체
+    // 문자열 배열을 순회하며 애너그램 그룹화
     for(let str of strs){
-        let s = str.split('').sort().join('')
-        if(!map[s]) map[s] = []
-        map[s].push(str)
+        // 문자열을 정렬하여 애너그램을 동일한 키로 만듦
+        let s = str.split('').sort().join('');
+        // 해당 키가 없으면 빈 배열로 초기화
+        if(!map[s]) map[s] = [];
+        // 애너그램 그룹에 현재 문자열 추가
+        map[s].push(str);
     }
-    return Object.values(map)
+    // 애너그램 단위로 그룹화된 배열 반환
+    return Object.values(map);
 };
