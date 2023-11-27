a315c8c3cb2a75a66465b491b6cff37abdc36ba9
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-28 01:47:02 +0800

    Update 0054-spiral-matrix.js

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -5,42 +5,42 @@
 var spiralOrder = function(matrix) {
     // MATRIX[Y][X]
     //    ---------
-    //Y0 | 1  2  3 |
-    //Y1 | 4  5  6 |
-    //Y2 | 7  8  9 |
+    // Y0 | 1  2  3 |
+    // Y1 | 4  5  6 |
+    // Y2 | 7  8  9 |
     //    ---------
-    //     X0 X1 X2
-   
-    const yLength = matrix.length, xLength = matrix[0].length;
-    let y=0, x=0, count=0, area= xLength * yLength, bag=[];
+    //      X0 X1 X2
 
-    while(count<area)
-    {
-        for(let i=x; count<area && i<xLength-x; i++)
-        {
-            bag.push(matrix[y][i])
+    const yLength = matrix.length, xLength = matrix[0].length;
+    let y = 0, x = 0, count = 0, area = xLength * yLength, bag = [];
+
+    while (count < area) {
+        // 위쪽 행
+        for (let i = x; count < area && i < xLength - x; i++) {
+            bag.push(matrix[y][i]);
             count++;
         }
         y++;
-        for(let i=y; count<area && i<yLength-y+1; i++)
-        {
-            bag.push(matrix[i][(xLength-1) - x])
+
+        // 오른쪽 열
+        for (let i = y; count < area && i < yLength - y + 1; i++) {
+            bag.push(matrix[i][(xLength - 1) - x]);
             count++;
         }
         x++;
 
-        for(let i= (xLength-1) - x; count<area && i>=x-1; i--)
-        {
-            bag.push(matrix[(yLength-1) - (y-1)][i])
+        // 아래쪽 행
+        for (let i = (xLength - 1) - x; count < area && i >= x - 1; i--) {
+            bag.push(matrix[(yLength - 1) - (y - 1)][i]);
             count++;
         }
 
-        for(let i= (yLength-1) - y; count<area && i>=y; i--)
-        {
-            bag.push(matrix[i][x-1])
+        // 왼쪽 열
+        for (let i = (yLength - 1) - y; count < area && i >= y; i--) {
+            bag.push(matrix[i][x - 1]);
             count++;
         }
-
     }
-    return bag
+
+    return bag;
 };
