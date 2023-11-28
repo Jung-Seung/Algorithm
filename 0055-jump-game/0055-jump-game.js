603756915344926067b47e9a81b9d14ffb3b3784
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-29 03:13:26 +0800

    Update 0055-jump-game.js

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,24 +3,22 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    // Base condition...
-    if(nums.length <= 1)
+    // 배열의 길이가 1 이하이면 항상 가능
+    if (nums.length <= 1)
         return true;
-    // To keep the maximum index that can be reached...
     let maximum = nums[0];
-    // Traverse all the elements through loop...
-    for(let i = 0; i < nums.length; i++){
-        //if there is no way to jump to next...
-        // so we should return false...
-        if(maximum <= i && nums[i] == 0) 
+    for (let i = 0; i < nums.length; i++) {
+        // 현재 위치에 도달했을 때 점프 거리가 0인 경우
+        if (maximum <= i && nums[i] === 0)
             return false;
-        //update the maximum jump...    
-        if(i + nums[i] > maximum){
+        // 현재 위치에서 최대로 이동할 수 있는 거리 갱신
+        if (i + nums[i] > maximum) {
             maximum = i + nums[i];
         }
-        //maximum is enough to reach the end...
-        if(maximum >= nums.length-1) 
+        // 최대로 이동할 수 있는 거리가 배열의 끝에 도달하면 가능
+        if (maximum >= nums.length - 1)
             return true;
     }
-    return false;   
+    // 마지막까지 끝에 도달하지 못하면 불가능
+    return false;
 };
