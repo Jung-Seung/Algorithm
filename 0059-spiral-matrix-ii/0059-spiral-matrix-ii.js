199699e14e232eb9cdd75d79cae2787b82b6e49f
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-29 05:41:15 +0800

    Update 0059-spiral-matrix-ii.js

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -3,49 +3,55 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    let save = Array.from(Array(n),()=>new Array(n).fill(0))
-    let count =1;
-    let left =0;
-    let right =n-1;
-    let bottom = n-1;
+    // 행렬을 저장할 2차원 배열 초기화
+    let save = Array.from(Array(n), () => new Array(n).fill(0));
+    let count = 1;
+    let left = 0;
+    let right = n - 1;
+    let bottom = n - 1;
     let top = 0;
     let dir = 0;
 
-    while(count <= n*n){
-        if(dir == 0){
-            for(let i =left; i<=right; i++){
+    // count가 n*n이 될 때까지 반복
+    while (count <= n * n) {
+        // 방향이 오른쪽일 때
+        if (dir === 0) {
+            for (let i = left; i <= right; i++) {
                 save[top][i] = count;
-                count++
+                count++;
             }
-            top++
-            dir++
+            top++;
+            dir++;
         }
-        if(dir == 1){
-            for(let i =top; i<=bottom; i++){
+        // 방향이 아래쪽일 때
+        if (dir === 1) {
+            for (let i = top; i <= bottom; i++) {
                 save[i][right] = count;
-                count++
+                count++;
             }
-            right--
-            dir++        
+            right--;
+            dir++;
         }
-        if(dir == 2){
-            for(let i =right; i>=left; i--){
+        // 방향이 왼쪽일 때
+        if (dir === 2) {
+            for (let i = right; i >= left; i--) {
                 save[bottom][i] = count;
-                count++
+                count++;
             }
-            bottom--
-            dir++        
+            bottom--;
+            dir++;
         }
-        if(dir == 3){
-            for(let i =bottom; i>=top; i--){
+        // 방향이 위쪽일 때
+        if (dir === 3) {
+            for (let i = bottom; i >= top; i--) {
                 save[i][left] = count;
-                count++
+                count++;
             }
-            left++
-            dir++        
+            left++;
+            dir++;
         }
+        // 방향 초기화
         dir = 0;
     }
-
     return save;
 };
