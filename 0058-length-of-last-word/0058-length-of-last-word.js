b29d0729485daf2398e1586276588e1bcf3189a2
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-29 05:32:24 +0800

    Update 0058-length-of-last-word.js

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,15 +2,11 @@
  * @param {string} s
  * @return {number}
  */
-/*
-To solve this problem, you can use the trim() method.
 
-The trim() method removes whitespace from both ends of a string and returns a new string, without modifying the original string.
-
-After that, we find the index of the last space (i.e., the space before the last word) and subtract this index from the total length of the string, adjusted for the index ("-1").
-*/
+// 주어진 문자열에서 마지막 단어의 길이를 반환하는 함수
 var lengthOfLastWord = function(s) {
+    // 주어진 문자열 양 끝의 공백을 제거한 문자열을 얻음
     let trimmedString = s.trim();
-    
+    // 마지막 단어의 길이를 계산하여 반환
     return trimmedString.length - trimmedString.lastIndexOf(' ') - 1;
 };
