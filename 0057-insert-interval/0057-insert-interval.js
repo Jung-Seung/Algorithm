216bf18577e4eea72b504b30db7bfe73165be75d
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-29 04:44:13 +0800

    Update 0057-insert-interval.js

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,25 +4,34 @@
  * @return {number[][]}
  */
 var insert = function (intervals, newInterval) {
+  // 새로운 간격의 시작과 끝을 추출
   let [start, end] = newInterval;
+  // 새로운 간격을 병합한 왼쪽과 오른쪽 간격을 담을 배열
   let left = [];
   let right = [];
-  
+
+  // 기존 간격들을 순회
   for (const interval of intervals) {
+    // 현재 간격의 시작과 끝을 추출
     const [first, last] = interval;
-	
-	// current interval is smaller than newInterval
-    if (last < start) left.push(interval);
-	
-	// current interval is larger than newInterval
-    else if (first > end) right.push(interval);
-	
-	// there is a overlap
+
+    // 현재 간격이 새로운 간격의 왼쪽에 있는 경우
+    if (last < start) {
+      left.push(interval);
+    }
+    // 현재 간격이 새로운 간격의 오른쪽에 있는 경우
+    else if (first > end) {
+      right.push(interval);
+    }
+    // 겹치는 부분이 있는 경우
     else {
+      // 겹치는 부분의 시작은 두 간격의 시작 중 작은 값
       start = Math.min(start, first);
+      // 겹치는 부분의 끝은 두 간격의 끝 중 큰 값
       end = Math.max(end, last);
     }
   }
-  
-  return [...left, [start, end], ...right]; 
+
+  // 왼쪽, 새로운 간격, 오른쪽 순서로 병합된 간격들을 반환
+  return [...left, [start, end], ...right];
 };
