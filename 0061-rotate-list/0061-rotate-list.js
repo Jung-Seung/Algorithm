1a446b294df67b573e164f4ed69fb4f6a4ca12c3
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-30 00:02:11 +0800

    Update 0061-rotate-list.js

--- 0061-rotate-list/0061-rotate-list.js
+++ 0061-rotate-list/0061-rotate-list.js
@@ -11,49 +11,50 @@
  * @return {ListNode}
  */
 /* 
-Algorithm Explanation: (Image attached in comments)
-1.  We count the total number of nodes in the list using count variable,
-2.  Now the total rotations we have to do can be restricted to k= k % count.
-3.  Now we move a pointer from head equal to k positions. This is necessary 
-for the next interesting logic
-4. Now since our ptr is k steps ahead then it will tell us the kth position from end
-i.e we iterate until ptr.next !==null and we move our prev pointer. Once ptr.next===null
-i.e ptr reaches the last node, out prev pointer will be exactly at the kth position from end
-Here kth position from end is nothing but the number of k rotations
-5. Now simply break the list after prev node. Last node i.e ptr will now point to head
-i.e ptr.next=head, next node of prev becomes our new head and finally prev becomes our 
-last node so prev.next=null
+알고리즘 설명:
+1. count 변수를 사용하여 목록의 총 노드 수를 계산합니다.
+2. 이제 수행해야 하는 총 회전 횟수는 k = k % count로 제한될 수 있습니다.
+3. 이제 ptr을 k 위치만큼 head에서 이동시킵니다. 이것은 다음에 나오는 흥미로운 로직에 필요합니다.
+4. 이제 ptr이 k 단계 앞에 있기 때문에 k번째 위치를 나타냅니다. 
+   즉, ptr.next !== null인 동안 반복하고 prev 포인터를 이동합니다. 
+   한 번 ptr.next === null이면, 즉 ptr이 마지막 노드에 도달하면 prev 포인터는 정확히 끝에서 k번째 위치에 있을 것입니다.
+5. 이제 prev 노드 다음에서 목록을 나눕니다. 마지막 노드인 ptr은 이제 head를 가리킵니다. 
+   즉, ptr.next = head가 되고, prev의 다음 노드는 새로운 헤드가되며, 
+   마지막으로 prev는 마지막 노드가되어 prev.next = null이 됩니다.
 */
+
 var rotateRight = function (head, k) {
-	if (!head) return head;
-	let count = 0,
-		ptr = head;
+    // 예외 처리: 빈 연결 리스트인 경우
+    if (!head) return head;
 
-	//Step 1 of the algo, count list nodes
-	while (ptr) {
-		count++;
-		ptr = ptr.next;
-	}
+    let count = 0,
+        ptr = head;
 
-	//Ste 2: Number of rotations are now restricted within limit
-	k = k % count;
-	let prev = head;
-	ptr = head;
+    // Step 1: 연결 리스트의 노드 개수를 센다
+    while (ptr) {
+        count++;
+        ptr = ptr.next;
+    }
 
-	//Step 3: Moving one pointer k positions ahead
-	while (k--) {
-		ptr = ptr.next;
-	}
+    // Step 2: 회전 횟수를 노드 개수로 나눈 나머지로 조정
+    k = k % count;
+    let prev = head;
+    ptr = head;
 
-	//Step 4: The actual magic, explained above
-	while (ptr.next) {
-		prev = prev.next;
-		ptr = ptr.next;
-	}
+    // Step 3: 포인터를 k만큼 우측으로 이동
+    while (k--) {
+        ptr = ptr.next;
+    }
 
-	//Step 5: Simply modifying the head and last node
-	ptr.next = head;
-	head = prev.next;
-	prev.next = null;
-	return head;
+    // Step 4: 실제로 우측으로 회전하는 부분
+    while (ptr.next) {
+        prev = prev.next;
+        ptr = ptr.next;
+    }
+
+    // Step 5: 헤드와 마지막 노드를 수정하여 회전 적용
+    ptr.next = head;
+    head = prev.next;
+    prev.next = null;
+    return head;
 };
