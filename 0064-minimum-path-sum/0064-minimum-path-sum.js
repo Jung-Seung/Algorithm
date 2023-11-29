2f9abc206bbfab3681295747207b67f90f331801
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-11-30 01:58:07 +0800

    Update 0064-minimum-path-sum.js

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -2,29 +2,44 @@
  * @param {number[][]} grid
  * @return {number}
  */
+
+// 그리드의 왼쪽 상단에서 오른쪽 하단으로 이동하는 경로 중 합이 최소가 되는 합을 계산하는 함수입니다.
 var minPathSum = function(grid) {
-    const minGrid = grid.map(row => row.map(_ => Infinity))
-    const m = grid.length - 1
-    const n = grid[0].length - 1 
-    
+    // 그리드의 크기를 가져와서 각 셀의 최소 비용을 저장할 배열을 생성합니다.
+    const minGrid = grid.map(row => row.map(_ => Infinity));
+    const m = grid.length - 1;
+    const n = grid[0].length - 1;
+
+    // 특정 셀에서부터 오른쪽 또는 아래로 이동하여 도달할 수 있는 최소 합을 계산하는 재귀 함수입니다.
     function getMin(x, y) {
-        let right = Infinity
-        let down = Infinity
+        let right = Infinity;
+        let down = Infinity;
+
+        // 목적지에 도달한 경우 현재 셀의 비용을 반환합니다.
         if (x === m && y === n) {
-            return grid[x][y]
+            return grid[x][y];
         }
+
+        // 이미 계산한 결과가 있다면 해당 결과를 반환합니다.
         if (minGrid[x][y] !== Infinity) {
-            return minGrid[x][y]
+            return minGrid[x][y];
         }
-        if (x !== m) { // go right
-            right = getMin(x+1, y)
+
+        // 오른쪽으로 이동할 수 있는 경우 최소 합을 계산합니다.
+        if (x !== m) {
+            right = getMin(x + 1, y);
         }
-        if (y !== n) { // go down 
-            down = getMin(x, y+1)
+
+        // 아래로 이동할 수 있는 경우 최소 합을 계산합니다.
+        if (y !== n) {
+            down = getMin(x, y + 1);
         }
-        minGrid[x][y] = Math.min(right, down) + grid[x][y]
-        return minGrid[x][y]
+
+        // 현재 셀의 최소 비용을 저장하고 반환합니다.
+        minGrid[x][y] = Math.min(right, down) + grid[x][y];
+        return minGrid[x][y];
     }
 
-    return getMin(0,0)
+    // 시작 지점인 왼쪽 상단 셀에서부터 최소 합을 계산하여 반환합니다.
+    return getMin(0, 0);
 };
