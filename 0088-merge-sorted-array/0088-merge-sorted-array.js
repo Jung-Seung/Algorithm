3df30cec5d835089a02bfadb5ded727a138db3ad
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-12-01 00:21:03 +0900

    Update 0088-merge-sorted-array.js

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,10 +6,13 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    for(let i = 0; i < nums1.length; i++) {
-        if(i >= m) {
-            nums1[i] = nums2[i-m];
+    // nums1의 모든 요소를 확인
+    for (let i = 0; i < nums1.length; i++) {
+        // m 이후의 위치에 있는 경우, nums2의 요소를 추가
+        if (i >= m) {
+            nums1[i] = nums2[i - m];
         }
     }
-    return nums1.sort((a,b) => a-b);
+    // nums1을 정렬하고 반환
+    return nums1.sort((a, b) => a - b);
 };
