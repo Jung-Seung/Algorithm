cc20ada302a0a10a8050850a4de9d4b6cb6be466
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-12-22 12:39:16 +0900

    Time: 72 ms (89.17%), Space: 44.9 MB (85.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -3,56 +3,45 @@
  * @param {string} t
  * @return {string}
  */
-function minWindow(s, t) {
-    // 각 문자의 등장 횟수를 저장하는 객체
-    const charCount = {};
-    // 타겟 문자열 t의 각 문자 등장 횟수를 기록
-    for (const char of t) {
-        charCount[char] = (charCount[char] || 0) + 1;
+var minWindow = function(s, t) {
+    // 만약 t의 길이가 s의 길이보다 크다면 빈 문자열 반환
+    if (t.length > s.length) return '';
+    // 필요한 문자의 개수를 저장하는 객체
+    const neededChars = {};
+    // t 문자열의 각 문자의 등장 횟수를 기록
+    for (let char of t) {
+        neededChars[char] = (neededChars[char] || 0) + 1;
     }
-    // 필요한 문자의 총 개수
-    let requiredChars = Object.keys(charCount).length;
-    // 윈도우의 시작과 끝 인덱스
+    // 윈도우의 왼쪽, 오른쪽 포인터 및 필요한 문자의 종류 수 초기화
     let left = 0;
     let right = 0;
-    // 최소 윈도우의 길이와 시작 인덱스
-    let minLength = Infinity;
-    let minWindowStart = 0;
-    // 남은 필요한 문자의 개수
-    let missingChars = 0;
-    // 문자열을 검사하는 동안의 반복문
+    let neededLength = Object.keys(neededChars).length;
+    // 결과로 반환할 최소 윈도우의 부분 문자열
+    let substring = '';
+    // 오른쪽 포인터를 이동하면서 탐색
     while (right < s.length) {
-        const char = s[right];
-        // 현재 문자가 필요한 문자에 속하는 경우
-        if (charCount[char] !== undefined) {
-            charCount[char]--;
-            // 필요한 문자를 다 찾은 경우
-            if (charCount[char] === 0) {
-                missingChars++;
-            }
-        }
+        const rightChar = s[right];
+        // 현재 문자의 필요한 개수를 감소하고, 필요한 문자 종류 수 업데이트
+        neededChars[rightChar]--;
+        if (neededChars[rightChar] === 0) neededLength--;
         // 모든 필요한 문자를 찾은 경우
-        while (missingChars === requiredChars) {
-            // 현재 윈도우의 길이가 최소인지 확인
-            if (right - left + 1 < minLength) {
-                minLength = right - left + 1;
-                minWindowStart = left;
+        while (neededLength === 0) {
+            // 현재까지의 부분 문자열이 더 작거나 처음인 경우 갱신
+            if (!substring || substring.length > right - left + 1) {
+                substring = s.slice(left, right + 1);
             }
             const leftChar = s[left];
-            // 윈도우의 왼쪽을 이동하면서 필요한 문자를 찾음
-            if (charCount[leftChar] !== undefined) {
-                charCount[leftChar]++;
-
-                // 필요한 문자를 놓친 경우
-                if (charCount[leftChar] > 0) {
-                    missingChars--;
-                }
+            // leftChar가 필요한 개수가 0이 되면, 다시 필요한 문자로 카운트
+            if (neededChars[leftChar] === 0) {
+                neededLength++;
             }
+            // leftChar의 필요한 개수를 증가시키고, 왼쪽 포인터 이동
+            neededChars[leftChar]++;
             left++;
         }
-        // 오른쪽으로 계속 확장
+        // 오른쪽 포인터 이동
         right++;
     }
-    // 최소 윈도우를 찾은 경우 반환, 그렇지 않으면 빈 문자열 반환
-    return minLength === Infinity ? "" : s.substr(minWindowStart, minLength);
-}
+    // 최소 윈도우 부분 문자열 반환
+    return substring;
+};
