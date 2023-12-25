1b9e7cb9e291abb4c3b80264421613e6c6fbb64e
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-12-26 05:19:04 +0900

    Time: 402 ms (100.00%), Space: 138 MB (46.92%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -4,27 +4,28 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
-    // 결과를 저장할 배열
+    // 최종 결과를 저장할 배열
     const result = [];
-    // 재귀 함수를 정의하여 조합을 생성
-    
-    function generateCombinations(start, currentCombination) {
-        // k개의 원소를 선택한 경우, 결과에 추가
-        if (currentCombination.length === k) {
-            result.push([...currentCombination]);
-            return;
-        }
-        // 현재 위치부터 n까지 반복
-        for (let i = start; i <= n; i++) {
-            // 현재 원소를 추가하고 재귀 호출
-            currentCombination.push(i);
-            generateCombinations(i + 1, currentCombination);
-            // 백트래킹: 현재 원소를 제거하여 다른 조합을 시도
-            currentCombination.pop();
-        }
-    }
-    // 재귀 함수 호출 시작
-    generateCombinations(1, []);
+    // 재귀 함수를 호출하여 조합 생성
+    generateCombinations(1, n, k, [], result);
     // 최종 결과 반환
     return result;
 };
+
+// 조합을 생성하는 재귀 함수
+function generateCombinations(start, n, k, combination, result) {
+    // k개의 숫자를 선택한 경우, 현재 조합을 결과에 추가
+    if (k === 0) {
+        result.push([...combination]);
+        return;
+    }
+    // 현재 위치부터 n까지 반복
+    for (let i = start; i <= n; ++i) {
+        // 현재 숫자를 조합에 추가
+        combination.push(i);
+        // 재귀 호출: 다음 숫자를 선택하도록 함
+        generateCombinations(i + 1, n, k - 1, combination, result);
+        // 백트래킹: 현재 숫자를 제거하여 다른 조합을 시도
+        combination.pop();
+    }
+}
