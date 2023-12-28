cf6740c5df7a1f7b024b5f6645bf39d05c63243f
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-12-29 05:07:13 +0900

    Time: 115 ms (5.01%), Space: 44 MB (50.15%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,33 +3,24 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // 예외 처리: 배열이 비어있을 경우 길이는 0
-    if (nums.length === 0) {
-        return 0;
+    // 예외 처리: 배열의 길이가 2 이하인 경우 이미 중복 허용 가능
+    if(nums.length <= 2) {
+        return nums.length;
     }
 
-    // 결과를 저장할 인덱스
-    let resultIndex = 1;
-    // 현재 숫자의 등장 횟수
-    let count = 1;
+    // 초기화: 배열의 첫 두 숫자는 항상 중복 허용
+    let k = 2;
 
-    // 배열을 순회하면서 중복을 최대 2번까지 허용하면서 정렬된 순서로 유지
-    for (let i = 1; i < nums.length; i++) {
-        // 현재 숫자가 이전 숫자와 같으면 count 증가
-        if (nums[i] === nums[i - 1]) {
-            count++;
-        } else {
-            // 현재 숫자가 이전 숫자와 다르면 count 초기화
-            count = 1;
-        }
-
-        // 중복이 최대 2번까지만 허용하므로 count가 2 이하일 때만 결과 배열에 추가
-        if (count <= 2) {
-            nums[resultIndex] = nums[i];
-            resultIndex++;
+    // 배열을 순회하면서 중복을 최대 2번까지만 허용하면서 정렬된 순서로 유지
+    for(let i = 2; i < nums.length; i++){
+        // 현재 인덱스의 숫자가 (k-1)번째와 (k-2)번째 숫자와 다를 때만 카운트
+        if(nums[i] !== nums[k - 2] || nums[i] !== nums[k - 1]){
+            nums[k] = nums[i];
+            k++;
+        // 현재 인덱스의 숫자가 (k-1)번째와 (k-2)번째 숫자와 같으면 스킵
         }
     }
 
-    // 결과 배열의 길이가 최종 k
-    return resultIndex;
+    // 수정된 배열의 길이 k 반환
+    return k;
 };
