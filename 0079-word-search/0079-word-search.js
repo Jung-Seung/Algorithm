c89f6d01c88b7f99a3c671dd2c898dd0ae1044cd
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2023-12-28 10:57:39 +0900

    Time: 1152 ms (29.94%), Space: 42.7 MB (65.09%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -3,53 +3,47 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
-    // 주어진 좌표 (i, j) 주변을 상, 하, 좌, 우로 이동하기 위한 방향
-    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+const exist = (board, word) => {
+  // 보드의 세로 길이
+  const h = board.length;
+  // 보드의 가로 길이
+  const w = board[0].length;
+  // 상, 우, 하, 좌 방향으로 이동하기 위한 배열
+  const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+  // DFS (깊이 우선 탐색) 함수
+  const go = (x, y, k) => {
+    // 현재 좌표의 값이 단어의 현재 인덱스와 일치하지 않으면 false 반환
+    if (board[x][y] !== word[k]) return false;
+    // 단어의 모든 문자를 찾았을 경우 true 반환
+    if (k === word.length - 1) return true;
+
+    // 현재 좌표를 '*'로 표시하여 방문했음을 표시
+    board[x][y] = '*';
     
-    // 주어진 좌표가 유효한지 확인하는 함수
-    const isValid = (i, j) => i >= 0 && i < board.length && j >= 0 && j < board[0].length;
-    
-    // DFS (깊이 우선 탐색)을 통해 단어를 찾는 함수
-    const search = (i, j, index) => {
-        // 현재 좌표의 문자가 단어의 현재 인덱스와 일치하는지 확인
-        if (board[i][j] !== word[index]) {
-            return false;
-        }
-
-        // 단어의 모든 문자를 찾았을 경우
-        if (index === word.length - 1) {
-            return true;
-        }
-
-        // 현재 좌표 방문 표시
-        const originalChar = board[i][j];
-        board[i][j] = '#';
-
-        // 상, 하, 좌, 우로 이동하여 다음 좌표에서 다시 검색
-        for (const [dx, dy] of directions) {
-            const ni = i + dx;
-            const nj = j + dy;
-            if (isValid(ni, nj) && search(ni, nj, index + 1)) {
-                return true;
-            }
-        }
-
-        // 현재 좌표 방문 표시 해제
-        board[i][j] = originalChar;
-
-        return false;
-    };
-
-    // 보드의 모든 좌표에서 탐색 시작
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            if (search(i, j, 0)) {
-                return true;
-            }
-        }
+    // 상, 우, 하, 좌로 이동하여 다음 좌표에서 다시 검색
+    for (const [dx, dy] of dirs) {
+      const i = x + dx;
+      const j = y + dy;
+      if (i >= 0 && i < h && j >= 0 && j < w) {
+        if (go(i, j, k + 1)) return true;
+      }
     }
-
-    // 단어를 찾지 못한 경우
+    
+    // 현재 좌표를 원래 값으로 복원
+    board[x][y] = word[k];
+    
+    // false 반환 (해당 경로에서 단어를 찾지 못한 경우)
     return false;
+  };
+
+  // 보드의 모든 좌표에서 DFS 탐색 시작
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (go(i, j, 0)) return true;
+    }
+  }
+
+  // 모든 좌표에서 단어를 찾지 못한 경우 false 반환
+  return false;
 };
