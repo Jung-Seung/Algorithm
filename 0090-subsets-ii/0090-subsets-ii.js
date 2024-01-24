ff41be3ac073e0971a3e1ab224dc678c54a9ac2f
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-01-24 18:46:02 +0800

    Time: 57 ms (78.02%), Space: 55 MB (5.16%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -2,43 +2,31 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 중복된 숫자가 포함된 정수 배열이 주어졌을 때, 중복되지 않은 모든 부분집합을 반환합니다.
 var subsetsWithDup = function(nums) {
-    // 결과를 저장할 배열
-    const result = [];
-    
-    // 중복된 부분집합을 방지하기 위해 정렬
-    nums.sort((a, b) => a - b);
-    
-    // 부분집합을 생성하는 재귀 함수
-    const generateSubsets = (index, currentSubset) => {
+    // 재귀적으로 중복되지 않은 부분집합을 찾는 함수.
+    function findSubset(arr, curr) {
         // 현재 부분집합을 결과에 추가
-        result.push([...currentSubset]);
-
-        // 중복을 방지하기 위해 이전에 선택한 숫자를 기록
-        let prevNum = null;
+        res.push([...curr]);
 
         // 숫자 배열을 순회하며 부분집합 생성
-        for (let i = index; i < nums.length; i++) {
-            // 중복된 숫자를 건너뛰기
-            if (nums[i] === prevNum) continue;
+        for (let i = 0; i < arr.length; i++) {
+            // 중복된 숫자를 건너뛰기 (처음 나오는 숫자   또는 바로 이전 숫자와 다를 때만 진행)
+            if (i === 0 || arr[i] !== arr[i - 1]) {
+                // 숫자를 현재 부분집합에 추가
+                curr.push(arr[i]);
 
-            // 숫자를 현재 부분집합에 추가
-            currentSubset.push(nums[i]);
+                // 재귀 호출로 다음 숫자 추가
+                findSubset(arr.slice(i + 1), curr);
 
-            // 재귀 호출로 다음 숫자 추가
-            generateSubsets(i + 1, currentSubset);
-
-            // 이전에 추가한 숫자 기록 업데이트
-            prevNum = nums[i];
-
-            // 현재 추가한 숫자를 부분집합에서 제거 (백트래킹)
-            currentSubset.pop();
+                // 현재 추가한 숫자를 부분집합에서 제거 (백트래킹)
+                curr.pop();
+            }
         }
-    };
+    }
 
-    // 부분집합 생성 시작
-    generateSubsets(0, []);
-
-    // 최종 결과 반환
-    return result;
+    let res = [];  // 최종 결과를 저장할 배열
+    nums.sort((a, b) => a - b);  // 중복을 방지하기 위해 배열 정렬
+    findSubset(nums, []);  // 부분집합 생성 시작
+    return res;  // 최종 결과 반환
 };
