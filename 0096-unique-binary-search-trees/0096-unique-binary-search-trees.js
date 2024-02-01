2b452834f4f53a82ab0c360e241e191cf7cbd524
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-02-02 07:19:39 +0800

    Time: 55 ms (34.08%), Space: 48.9 MB (6.28%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
@@ -2,33 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-// 주어진 정수 n에 대한 구조적으로 고유한 BST(이진 탐색 트리)의 개수를 반환하는 함수
 var numTrees = function(n) {
-    // 메모이제이션을 위한 배열 초기화
-    const memo = new Array(n + 1).fill(-1);
-    // 주어진 노드 수에 대한 BST 개수를 재귀적으로 계산하는 함수
-    const countBST = (nodes) => {
-        // 기저 사례: 노드 수가 0 또는 1일 때 경우의 수는 1
-        if (nodes <= 1) {
-            return 1;
-        }
-        // 이미 계산한 경우 메모값 반환
-        if (memo[nodes] !== -1) {
-            return memo[nodes];
-        }
-        let totalCount = 0;
-        // 루트 노드를 1부터 n까지 시도하며 가능한 모든 BST의 경우를 합산
-        for (let root = 1; root <= nodes; root++) {
-            const leftSubtrees = countBST(root - 1); // 왼쪽 서브트리의 경우의 수
-            const rightSubtrees = countBST(nodes - root); // 오른쪽 서브트리의 경우의 수
+    // 'sol' 배열을 생성하여 솔루션을 저장합니다. 초기값은 n=0, n=1인 경우입니다.
+    var sol = [1, 1];
+    // 2부터 n까지의 루프를 실행합니다.
+    for (let i = 2; i <= n; i++) {
+        sol[i] = 0;
 
-            // 현재 루트를 기준으로 가능한 BST의 경우의 수는 왼쪽 서브트리 * 오른쪽 서브트리
-            totalCount += leftSubtrees * rightSubtrees;
+        // 위의 루프 내에서 1부터 i까지의 루프를 실행합니다.
+        for (let j = 1; j <= i; j++) {
+            // 'sol' 배열의 i번째 위치를 업데이트하며 해당 인덱스의 곱을 더합니다.
+            sol[i] += sol[i - j] * sol[j - 1];
         }
-        // 메모이제이션
-        memo[nodes] = totalCount;
-        return totalCount;
-    };
-    // 주어진 노드 수에 대한 BST 개수 계산 시작
-    return countBST(n);
+    }
+    // 'sol' 배열의 n번째 인덱스 값을 반환하여 해를 얻습니다.
+    return sol[n];
 };
