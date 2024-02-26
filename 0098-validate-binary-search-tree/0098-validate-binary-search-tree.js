5998436d12f14a5233c28af8fd11e236eab0856d
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-02-26 18:10:41 +0900

    Time: 55 ms (88.73%), Space: 54.2 MB (8.79%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,17 +12,22 @@
  */
 var isValidBST = function(root) {
     // 이진 탐색 트리의 유효성을 검사하는 helper 함수
-    function isValid(node, lower, upper) {
+    var validate = function(node, lower, upper) {
         // 노드가 null인 경우 유효한 이진 탐색 트리의 일부로 간주되지 않음
-        if (!node) return true;
+        if (node == null) {
+            return true;
+        }
         
-        // 현재 노드의 값이 하한 값보다 작거나 상한 값보다 크면 유효하지 않음
-        if (node.val <= lower || node.val >= upper) return false;
-        
-        // 왼쪽 서브트리와 오른쪽 서브트리의 유효성을 재귀적으로 확인
-        return isValid(node.left, lower, node.val) && isValid(node.right, node.val, upper);
-    }
+        // 현재 노드의 값이 하한 값보다 크고 상한 값보다 작으면 유효한 이진 탐색 트리의 일부로 간주됨
+        if (lower < node.val && node.val < upper) {
+            // 왼쪽 서브트리와 오른쪽 서브트리의 유효성을 재귀적으로 확인
+            return validate(node.left, lower, node.val) && validate(node.right, node.val, upper);
+        } else {
+            // 범위를 벗어나는 경우 유효한 이진 탐색 트리의 일부로 간주되지 않음
+            return false;
+        }
+    };
     
     // helper 함수 호출하여 루트 노드부터 유효성 검사 시작
-    return isValid(root, -Infinity, Infinity);
+    return validate(root, -Infinity, Infinity);
 };
