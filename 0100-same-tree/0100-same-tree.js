e459a77493f36a88b54eee3a736cf924ba7a3906
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-02-28 15:03:36 +0900

    Time: 62 ms (11.42%), Space: 49.2 MB (33.41%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -16,9 +16,8 @@ var isSameTree = function(p, q) {
     if (!p && !q) return true;
     // 둘 중 하나만 null이면 동일하지 않다고 판단
     if (!p || !q) return false;
-    // 노드의 값이 서로 다르면 동일하지 않다고 판단
+    // 현재 노드의 값이 서로 다르면 동일하지 않다고 판단
     if (p.val !== q.val) return false;
-    
-    // 왼쪽 서브트리와 오른쪽 서브트리를 재귀적으로 비교
+    // 왼쪽 서브트리와 오른쪽 서브트리를 재귀적으로 비교하여 결과 반환
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
