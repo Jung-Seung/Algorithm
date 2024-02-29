978a3746f042e0bee55abe611d0cfbdd6a46e544
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-02-29 19:25:28 +0900

    Time: 99 ms (36.72%), Space: 49.2 MB (60.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,20 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // 숫자와 해당 숫자의 인덱스를 저장하는 Map을 생성합니다.
-    const map = new Map();
-    // 배열의 각 요소를 반복합니다.
-    for (let i = 0; i < nums.length; i++) {
-        // 현재 요소에 대한 보수(complement)를 계산합니다.
-        const complement = target - nums[i];
-        // Map에 보수가 이미 존재하는지 확인합니다.
-        if (map.has(complement)) {
-            // 보수가 존재한다면, 현재 숫자와 보수의 인덱스를 반환합니다.
-            return [map.get(complement), i];
+    // 배열을 순회하며 두 수의 합이 타겟과 같은지 확인
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            // 두 수의 합이 타겟과 같으면 해당 인덱스 반환
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-        // 보수가 존재하지 않는다면, 현재 숫자와 해당 숫자의 인덱스를 Map에 저장합니다.
-        map.set(nums[i], i);
     }
-    // 여기까지 왔다면, 조건을 만족하는 숫자 쌍이 없으므로 빈 배열을 반환합니다.
-    return [];
+    return []; // 정답이 없는 경우 빈 배열 반환
 };
