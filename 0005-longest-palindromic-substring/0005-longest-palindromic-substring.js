c9720494c32f3ed06eed586ed16380eba80ea741
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-01 15:05:06 +0900

    Time: 67 ms (93.64%), Space: 51 MB (60.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,37 +3,45 @@
  * @return {string}
  */
 var check = function(s, i, j) {
-    // i가 j보다 작은 동안 계속 반복
     while (i < j) {
-        // 문자열의 i번째와 j번째 문자를 비교하여 다르면 false 반환
         if (s[i] !== s[j]) {
             return false;
         }
-        // i를 증가, j를 감소하여 다음 문자 비교
         i++;
         j--;
     }
-    // 모든 비교에서 다르지 않았으면 true 반환 (팰린드롬)
     return true;
 }
 
 var longestPalindrome = function(s) {
-    const n = s.length; // 문자열의 길이
-    let starting_index = 0; // 팰린드롬 부분 문자열의 시작 인덱스
-    let max_len = 0; // 가장 긴 팰린드롬 부분 문자열의 길이
-    // 이중 반복문을 통해 모든 부분 문자열에 대해 팰린드롬 여부를 확인
-    for (let i = 0; i < n; i++) {
-        for (let j = i; j < n; j++) {
-            // check 함수를 사용하여 부분 문자열이 팰린드롬인지 확인
-            if (check(s, i, j)) {
-                // 현재 부분 문자열이 이전까지 찾은 가장 긴 팰린드롬보다 길면 업데이트
-                if (j - i + 1 > max_len) {
-                    max_len = j - i + 1;
-                    starting_index = i;
-                }
-            }
+    return s.split('').reduce((acc, cur, idx, arr) => {
+        let evenOffset = 0; // 짝수 길이 팰린드롬 검사를 위한 오프셋
+        let oddOffset = 0; // 홀수 길이 팰린드롬 검사를 위한 오프셋
+        
+        // 현재 문자를 중심으로 하는 짝수 길이 팰린드롬 검사
+        while(evenOffset <= idx && idx + 1 + evenOffset < arr.length && 
+              arr[idx - evenOffset] === arr[idx + 1 + evenOffset]){
+            evenOffset++;
         }
-    }
-    // 가장 긴 팰린드롬 부분 문자열을 추출하여 반환
-    return s.substring(starting_index, starting_index + max_len);
+        
+        // 현재 문자를 중심으로 하는 홀수 길이 팰린드롬 검사
+        while(oddOffset < idx && idx + 1 + oddOffset < arr.length && 
+              arr[idx - 1 - oddOffset] === arr[idx + 1 + oddOffset]){
+            oddOffset++;
+        }
+
+        // 짝수 길이와 홀수 길이 팰린드롬의 길이 계산
+        const evenLength = evenOffset * 2;
+        const oddLength = oddOffset ? oddOffset * 2 + 1 : 1;
+        
+        // 현재까지의 최장 팰린드롬과 비교하여 최장 팰린드롬 갱신
+        if(acc.length > evenLength && acc.length > oddLength){
+            return acc; // 현재까지의 최장 팰린드롬이 더 긴 경우, 그대로 반환
+        }
+        else if(evenLength > oddLength){
+            return s.substr(idx - evenOffset + 1, evenLength); // 짝수 길이 팰린드롬이 더 긴 경우, 해당 부분 문자열 반환
+        }else{
+            return s.substr(idx - oddOffset, oddLength); // 홀수 길이 팰린드롬이 더 긴 경우, 해당 부분 문자열 반환
+        }
+    },''); // 초기값으로 빈 문자열을 사용하여 reduce 시작
 }
