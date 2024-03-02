da4e8908b806ec105c931b52918c475bd9613465
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-02 19:14:58 +0900

    Time: 84 ms (63.96%), Space: 54.4 MB (39.73%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -4,28 +4,27 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    // 주어진 문자열이 행 수보다 짧거나 행 수가 2보다 작으면 원래 문자열 반환
-    if(s.length <= numRows || numRows < 2) return s;
-
+    // 주어진 행 수가 1인 경우, 원래 문자열 그대로 반환
+    if (numRows === 1) {
+        return s;
+    } 
+    // 결과 문자열을 저장할 변수
+    let result = '';
     // 주어진 문자열의 길이
-    const len = s.length;
-    // 주어진 행 수를 기반으로 각 문자가 위치할 행을 계산하기 위한 값
-    const num = 2 * (numRows - 1);
-    // 각 행의 문자들을 저장할 배열
-    let res = Array(numRows).fill('');
-    // 문자열을 탐색하기 위한 임시 변수
-    let tmp = 0;
-    // 문자열을 순회하면서 각 문자가 위치할 행을 계산하여 결과에 저장
-    for(let i = 0; i<len; i++){
-        // 현재 문자가 위치할 행 계산
-        tmp = i % num;
-        // 각 행에 문자 추가
-        if(tmp < numRows){
-            res[tmp] += s[i]; // 직선으로 채워지는 부분
-        } else {
-            res[num - tmp] += s[i]; // 대각선으로 채워지는 부분
+    const n = s.length;
+    // 주기 길이
+    const cycleLen = 2 * numRows - 2;
+    // 각 행마다 문자를 추가하며 변환
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j + i < n; j += cycleLen) {
+            // 직선 부분에 대해 문자 추가
+            result += s[j + i];
+            // 대각선 부분에 대해 문자 추가
+            if (i !== 0 && i !== numRows - 1 && j + cycleLen - i < n) {
+                result += s[j + cycleLen - i];
+            }
         }
     }
-    // 각 행에 저장된 문자열을 합쳐서 반환
-    return res.join('');
+    // 결과 문자열 반환
+    return result;
 };
