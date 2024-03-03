0f2e1a70bf7a97fbe5ed67bf80823bef929cc660
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-04 03:11:21 +0900

    Time: 66 ms (81.40%), Space: 52.8 MB (47.58%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -3,36 +3,27 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    s = s.trim();  // 선행 및 후행 공백 제거
-
+    s = s.trim();  // 앞뒤 공백 제거
     if (s.length === 0) {
         return 0;  // 빈 문자열 처리
     }
-
-    let num = 0;  // 결과를 저장할 변수 초기화
-    let i = 0;    // 문자열 인덱스 초기화
-    let sign = 1;  // 부호 저장, 양수면 1, 음수면 -1
-
-    // 부호 확인
+    let num = 0;  // 변환된 숫자 저장
+    let i = 0;  // 문자열 인덱스
+    let sign = 1;  // 양수면 1, 음수면 -1
+    // 부호 처리
     if (s[i] === '+') {
-        i++;
+        i++;  // 양수 부호일 경우 다음 문자 확인
     } else if (s[i] === '-') {
-        i++;
-        sign = -1;
+        i++;  // 음수 부호일 경우 다음 문자 확인
+        sign = -1;  // 음수 부호 설정
     }
-
-    // 숫자 추출
+    // 숫자 변환
     while (i < s.length && /^\d$/.test(s[i])) {
-        num = num * 10 + parseInt(s[i]);
-        i++;
+        num = num * 10 + parseInt(s[i]);  // 현재 문자를 숫자로 변환하여 추가
+        i++;  // 다음 문자 확인
     }
-
-    // 부호 적용
-    num *= sign;
-
-    // 정수 오버플로우 체크
+    num *= sign;  // 부호 적용
+    // 32비트 정수 범위 검사하여 최댓값 또는 최솟값 반환
     num = Math.max(Math.min(num, Math.pow(2, 31) - 1), -Math.pow(2, 31));
-
-    // 최종 변환된 정수 반환
-    return num;
-};
+    return num;  // 변환된 정수 반환
+}
