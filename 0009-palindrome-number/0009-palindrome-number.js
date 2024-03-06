bd222ccd54d2381fc8402f7cd0adff3bdfd8d4d6
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-06 14:41:02 +0900

    Time: 116 ms (94.58%), Space: 58.1 MB (36.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,19 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    // 음수 또는 10의 배수인 경우 회문이 아님
-    if (x < 0 || (x !== 0 && x % 10 === 0)) {
+    const MinNum = -2147483648;  // 최소 정수 값
+    const MaxNum = 2147483647;   // 최대 정수 값
+    // 숫자가 정수 범위를 벗어나면 팰린드롬이 아님
+    if(x < MinNum || x > MaxNum) {
         return false;
     }
-
-    var half = 0;  // 숫자의 뒷부분을 저장할 변수 초기화
-
-    // 주어진 숫자를 반으로 나누어 뒤집은 뒷부분 생성
-    while (x > half) {
-        half = half * 10 + x % 10;
-        x = Math.floor(x / 10);
-    }
-
-    // 홀수 자릿수를 가진 경우 중앙값은 무시하여 비교
-    return x === half || x === Math.floor(half / 10);
+    // 주어진 숫자를 문자열로 변환하여 뒤집은 후 원래 숫자와 비교하여 팰린드롬 여부 확인
+    const reverse =  x.toString().split("").reverse().join("");
+    return x === Number(reverse);
 };
