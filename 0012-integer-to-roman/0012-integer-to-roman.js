f3ff7017474e9feeb8fa7fd70eaf6a71127d7d60
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-08 10:49:37 +0900

    Time: 104 ms (81.76%), Space: 53.5 MB (79.34%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,31 +3,11 @@
  * @return {string}
  */
 const intToRoman = function(num) {
-    // 로마 숫자에 대한 값의 맵
-    const value = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
-    };
-    
-    let result = ''; // 변환된 로마 숫자를 저장할 변수
-    
-    // 로마 숫자에 대한 값의 맵을 순회하면서 변환 과정 수행
-    for (let s of Object.keys(value)) {
-        const r = Math.floor(num / value[s]); // 현재 로마 숫자의 갯수 계산
-        num -= r * value[s]; // 남은 정수 계산
-        result += s.repeat(r); // 현재 로마 숫자를 결과에 추가
-    }
-    
-    return result; // 변환된 로마 숫자 반환
+    // 각 자릿수에 대한 로마 숫자 배열
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const ths = ["", "M", "MM", "MMM"];
+    // 각 자릿수에 대한 로마 숫자를 조합하여 반환
+    return ths[Math.floor(num / 1000)] + hrns[Math.floor((num % 1000) / 100)] + tens[Math.floor((num % 100) / 10)] + ones[num % 10];
 };
