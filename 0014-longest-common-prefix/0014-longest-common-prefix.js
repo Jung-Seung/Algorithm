23e20f44b78dddbc712767a9a16aca7470f09da3
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-13 17:10:27 +0900

    Time: 53 ms (71.01%), Space: 49.1 MB (67.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,19 +4,24 @@
  */
 var longestCommonPrefix = function(strs) {
     // 만약 문자열 배열이 비어있다면 빈 문자열 반환
-    if (!strs.length) return '';
+    if (strs.length === 0) {
+        return '';
+    }
+    // 첫 번째 문자열을 기준으로 초기화
+    let ans = strs[0];
     
-    // 첫 번째 문자열을 기준으로 각 문자열의 같은 위치의 문자를 비교하여 공통 접두사를 찾음
-    for (let i = 0; i < strs[0].length; i++) {
-        // 첫 번째 문자열의 i번째 문자를 기준으로 나머지 문자열들의 i번째 문자를 비교
-        for (let s of strs) {
-            // 만약 현재 문자열의 i번째 문자가 첫 번째 문자열의 i번째 문자와 다르다면,
-            // 해당 위치까지의 문자열을 반환하여 공통 접두사를 구함
-            if (s[i] !== strs[0][i]) {
-                return s.slice(0, i);
+    // 두 번째 문자열부터 순회하며 공통 접두사 찾기
+    for (let i = 1; i < strs.length; i++) {
+        // 현재 문자열이 기준 문자열로 시작하지 않을 때까지 반복하여 접두사 찾기
+        while (strs[i].indexOf(ans) !== 0) {
+            // 기준 문자열을 하나씩 줄여가며 다시 비교
+            ans = ans.substring(0, ans.length - 1);
+            // 만약 기준 문자열이 빈 문자열이 되면 공통 접두사 없음을 반환
+            if (ans === '') {
+                return '';
             }
         }
-    }   
-    // 모든 문자열이 같은 경우 첫 번째 문자열 자체가 공통 접두사가 되므로 첫 번째 문자열 반환
-    return strs[0];
+    }
+    // 모든 문자열을 검사한 후 공통 접두사 반환
+    return ans;
 };
