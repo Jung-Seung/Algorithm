e56a90053035f095297bd69298eae876b0c97c5d
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-20 03:25:06 +0900

    Time: 55 ms (64.14%), Space: 50.6 MB (46.46%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,23 +11,30 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    // fast와 slow라는 두 포인터를 머리에 초기화합니다.
-    let fast = head, slow = head;
-    // fast 포인터를 n+1번째 위치로 이동합니다.
-    for (let i = 0; i < n; i++) {
-        fast = fast.next;
+    // 더미 헤드 노드를 생성하여 연결 리스트의 시작을 나타냅니다.
+    const root = new ListNode(0);
+    root.next = head;
+    
+    // 두 개의 포인터를 설정합니다.
+    let front = root; // n번째 노드의 다음 노드를 가리킵니다.
+    let back = root; // n번째 노드를 가리킵니다.
+    
+    // front 포인터를 n번째 노드까지 이동시킵니다.
+    while (n >= 0) {
+        front = front.next;
+        n--;
     }
-    // 만약 fast가 null이라면, 제거할 노드가 머리 자체입니다.
-    if (!fast) {
-        return head.next;
+    
+    // front 포인터를 연결 리스트의 끝까지 이동시키고,
+    // back 포인터를 끝에서부터 n번째 노드의 이전 노드까지 이동시킵니다.
+    while (front) {
+        front = front.next;
+        back = back.next;
     }
-    // fast가 끝에 도달할 때까지 양 포인터를 함께 이동시킵니다.
-    while (fast.next) {
-        fast = fast.next;
-        slow = slow.next;
-    }
-    // 끝에서 n번째 노드를 제거하고, 그 전 노드의 next 포인터를 조정합니다.
-    slow.next = slow.next.next;
-    // 수정된 연결 리스트의 머리를 반환합니다.
-    return head;
+
+    // back 노드의 다음 노드를 건너뛰어 해당 노드를 제거합니다.
+    back.next = back.next.next;
+    
+    // 더미 헤드 노드를 제외한 연결 리스트를 반환합니다.
+    return root.next;
 };
