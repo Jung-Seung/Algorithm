d1ce447d3d24854cafaf444ad94541f7ffd70ad1
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-21 02:47:26 +0900

    Time: 49 ms (91.79%), Space: 50.5 MB (37.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,31 +2,30 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {   
-    // 괄호를 저장할 스택을 초기화합니다.
+var isValid = function(s) {
+    // 스택을 초기화합니다.
     const stack = [];
     
-    // 문자열을 순회하면서 괄호를 처리합니다.
-    for (let i = 0 ; i < s.length ; i++) {
+    // 문자열을 순회합니다.
+    for (let i = 0; i < s.length; i++) {
         let c = s.charAt(i);
         switch(c) {
-            case '(': 
-                stack.push(')');
+            // 여는 괄호가 나오면 해당하는 닫는 괄호를 스택에 추가합니다.
+            case '(': stack.push(')');
                 break;
-            case '[': 
-                stack.push(']');
+            case '[': stack.push(']');
                 break;
-            case '{': 
-                stack.push('}');
+            case '{': stack.push('}');
                 break;
+            // 닫는 괄호가 나오면 스택에서 마지막으로 추가된 여는 괄호와 일치하는지 확인합니다.
             default:
-                // 스택에서 팝한 값이 현재 처리중인 값과 다를 경우 유효하지 않은 괄호 문자열입니다.
                 if (c !== stack.pop()) {
                     return false;
                 }
         }
     }
     
-    // 스택의 길이가 0이면 유효한 괄호 문자열입니다.
+    // 스택이 비어있으면 모든 괄호 쌍이 일치하는 것으로 판단하여 true를 반환합니다.
+    // 그렇지 않으면 false를 반환합니다.
     return stack.length === 0;
 };
