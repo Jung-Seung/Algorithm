aedc4a5e1c43dfca6afae897cb3add5402b14063
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-26 17:09:11 +0900

    Time: 57 ms (41.13%), Space: 50.4 MB (58.71%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -2,34 +2,29 @@
  * @param {number} n
  * @return {string[]}
  */
-const generateParenthesis = (n) => {
-    // 생성된 모든 조합을 저장할 배열입니다.
-    const combinations = [];
-    
-    // DFS를 이용하여 유효한 괄호 조합을 생성합니다.
-    const dfs = (left, right, combination) => {
-        // 괄호 쌍이 모두 사용된 경우, 현재 조합을 배열에 추가하고 함수를 종료합니다.
-        if (left === n && right === n) {
-            combinations.push(combination);
-            return;
-        }
-        
-        // 왼쪽 괄호가 오른쪽 괄호보다 많은 경우
-        if (left > right) {
-            // 왼쪽 괄호를 추가할 수 있는 경우 추가합니다.
-            left < n && dfs(left + 1, right, combination + '(');
-            // 오른쪽 괄호를 추가할 수 있는 경우 추가합니다.
-            right < n && dfs(left, right + 1, combination + ')');
-        }
-        // 왼쪽 괄호와 오른쪽 괄호의 수가 같은 경우
-        else if (left === right) {
-            // 왼쪽 괄호를 추가할 수 있는 경우 추가합니다.
-            left < n && dfs(left + 1, right, combination + '(');
-        }
-    }
-    
-    // DFS를 시작합니다. 초기값은 0으로 설정합니다.
-    dfs(0, 0, '');
-    // 생성된 모든 유효한 조합을 반환합니다.
-    return combinations;
+// 주어진 숫자 n에 대한 모든 유효한 괄호 조합을 생성합니다.
+var generateParenthesis = function(n) {
+  // 생성된 모든 유효한 괄호 조합을 저장할 배열입니다.
+  const result = [];
+  // 유효한 괄호 조합을 생성하는 보조 함수를 호출합니다.
+  generateParentheses(result, '', 0, 0, n);
+  // 생성된 모든 유효한 괄호 조합을 반환합니다.
+  return result;
+};
+
+// 유효한 괄호 조합을 생성하는 보조 함수입니다.
+const generateParentheses = (result, current, open, close, n) => {
+  // 괄호 쌍의 개수가 2*n이 되면 현재 조합을 결과 배열에 추가하고 종료합니다.
+  if (current.length === 2 * n) {
+    result.push(current);
+    return;
+  }
+  // 여는 괄호를 추가할 수 있는 경우 추가합니다.
+  if (open < n) {
+    generateParentheses(result, current + '(', open + 1, close, n);
+  }
+  // 닫힌 괄호를 추가할 수 있는 경우 추가합니다.
+  if (close < open) {
+    generateParentheses(result, current + ')', open, close + 1, n);
+  }
 };
