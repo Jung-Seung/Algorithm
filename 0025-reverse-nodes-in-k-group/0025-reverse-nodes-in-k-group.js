cf6cc7a192ba17fbe426603d4183b92ca4104692
이정승 <112936736+Jung-Seung@users.noreply.github.com>
2024-03-29 00:31:21 +0900

    Time: 77 ms (20.78%), Space: 54 MB (7.73%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -10,35 +10,53 @@
  * @param {number} k
  * @return {ListNode}
  */
+// 주어진 연결 리스트를 k개의 노드를 갖는 그룹으로 나눈 뒤, 각 그룹 내의 노드 순서를 뒤집는 함수입니다.
 const reverseKGroup = function(head, k) {
-    // 입력이 없으면 null을 반환합니다.
-    if (!head) return null;
+    // 더미 노드를 생성하고, 이전 그룹의 꼬리를 가리키도록 설정합니다.
+    var dummy = new ListNode(0);
+    dummy.next = head;
+    var prevGroupTail = dummy;
 
-    // 연결 리스트를 배열로 변환하여 순서를 뒤집습니다.
-    let list = [];
-    while (head !== null) {
-        list.push(head.val);
+    // 연결 리스트를 그룹으로 나누고 각 그룹을 순회합니다.
+    while (head) {
+        // 그룹의 시작 노드와 끝 노드를 찾습니다.
+        var groupStart = head;
+        var groupEnd = getGroupEnd(head, k);
+
+        // 만약 그룹의 끝이 없다면, 그룹 나누기를 중단합니다.
+        if (!groupEnd)
+            break;
+
+        // 그룹 내의 노드 순서를 뒤집고, 그룹의 시작 부분을 연결합니다.
+        prevGroupTail.next = reverseList(groupStart, groupEnd.next);
+        // 이전 그룹의 꼬리를 현재 그룹의 시작 부분으로 갱신합니다.
+        prevGroupTail = groupStart;
+        // 다음 그룹의 시작 노드를 설정합니다.
+        head = prevGroupTail.next;
+    }
+
+    // 더미 노드 다음의 연결 리스트가 뒤집힌 순서를 갖는 새로운 헤드 노드입니다.
+    var newHead = dummy.next;
+    return newHead;
+}
+
+// 주어진 노드부터 k개의 노드를 순회하여 그룹의 끝 노드를 반환하는 함수입니다.
+var getGroupEnd = function(head, k) {
+    while (head && k > 1) {
         head = head.next;
+        k--;
     }
+    return head;
+}
 
-    // 연결 리스트가 비어있으면 null을 반환합니다.
-    if (list.length === 0) return null;
-
-    // k개의 노드로 이루어진 그룹을 순회하면서 노드의 순서를 뒤집습니다.
-    for (let i = 0; i < list.length; i += k) {
-        let temp = list.slice(i, i+k);
-        if (temp.length < k) continue;
-        for (let j = 0; j < k; j++) {
-            list[i+j] = temp[k-j-1];
-        }
+// 주어진 노드부터 stop 노드까지의 연결 리스트를 뒤집는 함수입니다.
+var reverseList = function(head, stop) {
+    var prev = stop;
+    while (head !== stop) {
+        var next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
     }
-
-    // 뒤집힌 순서의 노드로부터 새로운 연결 리스트를 구성합니다.
-    let result = new ListNode(list.pop());
-    while (list.length > 0) {
-        result = new ListNode(list.pop(), result);
-    }
-
-    // 새로운 연결 리스트의 헤드 노드를 반환합니다.
-    return result;
-};
+    return prev;
+}
